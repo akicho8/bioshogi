3f881c24f0adfebea4c507994c2ab3f8d05cb50f
akicho8 <akicho8@gmail.com>
2013-06-18 00:37:16 +0900

    SoldierMove のテスト追加

--- spec/mini_soldier_spec.rb
+++ spec/mini_soldier_spec.rb
@@ -12,4 +12,13 @@ module Bushido
       MiniSoldier[point: Point["５一"], piece: Piece["玉"]].to_s.should == "5一玉"
     end
   end
+
+  describe SoldierMove do
+    it "#to_hand" do
+      SoldierMove[point: Point["１三"], piece: Piece["銀"], origin_soldier: MiniSoldier.from_str("１四銀"), promoted_trigger: true].to_hand.should == "1三銀成(14)"
+    end
+  end
+
+  # describe PieceStake do
+  # end
 end
