51181ce4ba3da410129727f31980c85e59df988b
akicho8 <akicho8@gmail.com>
2017-11-14 20:02:29 +0900

    saferenum -x examples

---
+++ examples/0190_ki2のテスト.rb
@@ -0,0 +1,26 @@
+require "./example_helper"
+
+def assert_equal(a, b)
+  if a == b
+    puts "OK"
+  else
+    puts "ERROR"
+  end
+end
+
+files = Pathname.glob("../resources/竜王戦_ki2/*.ki2")
+files.each do |file|
+  puts file
+
+  info = Parser.parse_file(file)
+  kif_str = info.to_kif
+  ki2_str = info.to_ki2
+
+  v = Parser.parse(kif_str)
+  assert_equal(v.to_kif, kif_str)
+  assert_equal(v.to_ki2, ki2_str)
+
+  v = Parser.parse(ki2_str)
+  assert_equal(v.to_kif, kif_str)
+  assert_equal(v.to_ki2, ki2_str)
+end
