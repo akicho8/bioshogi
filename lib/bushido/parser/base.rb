3cc7e99388026a56a995b869f4e19fb666dbfa9d
akicho8 <akicho8@gmail.com>
2017-11-17 01:36:01 +0900

    KI2の棋譜の並びを縦毎に揃えるように改善

--- lib/bushido/parser/base.rb
+++ lib/bushido/parser/base.rb
@@ -1,7 +1,9 @@
 # -*- compile-command: "bundle exec rspec ../../spec/kif_format_spec.rb" -*-
 
-require "time"
-require "kconv"
+require "time"                  # for Time.parse
+require "kconv"                 # for toeuc
+
+require "active_support/core_ext/array/grouping" # for in_groups_of
 
 module Bushido
   module Parser
@@ -231,7 +233,7 @@ module Bushido
         def to_ki2(**options)
           options = {
             cols: 10,
-            length: 11,
+            # length: 11,
             same_suffix: "　",
           }.merge(options)
 
@@ -240,13 +242,33 @@ module Bushido
             header_write(out)
             out << "\n"
           end
-          out << mediator.hand_logs.group_by.with_index{|_, i| i / options[:cols] }.values.collect { |v|
-            v.collect { |e|
-              s = e.to_s_ki2(with_mark: true, same_suffix: options[:same_suffix])
-              mb_ljust(s, options[:length])
-            }.join.strip + "\n"
-          }.join
-          out << mediator.last_message
+
+          if false
+            out << mediator.hand_logs.group_by.with_index{|_, i| i / options[:cols] }.values.collect { |v|
+              v.collect { |e|
+                s = e.to_s_ki2(with_mark: true, same_suffix: options[:same_suffix])
+                mb_ljust(s, options[:length])
+              }.join.strip + "\n"
+            }.join
+          else
+            list = mediator.hand_logs.collect do |e|
+              e.to_s_ki2(with_mark: true, same_suffix: options[:same_suffix])
+            end
+
+            list2 = list.in_groups_of(options[:cols])
+            column_widths = list2.transpose.collect do |e|
+              e.collect { |e| e.to_s.toeuc.bytesize }.max
+            end
+
+            list2 = list2.collect do |a|
+              a.collect.with_index do |e, i|
+                mb_ljust(e.to_s, column_widths[i])
+              end
+            end
+            out << list2.collect { |e| e.join(" ").strip + "\n" }.join
+          end
+
+          out << mediator.last_message + "\n"
           out
         end
 
