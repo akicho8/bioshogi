dbe95fde0cded244b542c8dd1fed6faa3bcdeb74
akicho8 <akicho8@gmail.com>
2017-11-18 22:48:46 +0900

    手合いを判断する例を追加

---
+++ examples/0312_盤面_手合いを判断する方法.rb
@@ -0,0 +1,22 @@
+# 盤面の読み取り
+require "./example_helper"
+
+info1 = Parser.board_parse(<<~EOT)
++---------------------------+
+|v香v桂v銀v金v玉v金v銀v桂v香|一
+| ・v飛 ・ ・ ・ ・ ・v角 ・|二
+|v歩v歩v歩v歩v歩v歩v歩v歩v歩|三
+| ・ ・ ・ ・ ・ ・ ・ ・ ・|四
+| ・ ・ ・ ・ ・ ・ ・ ・ ・|五
+| ・ ・ ・ ・ ・ ・ ・ ・ ・|六
+| 歩 歩 歩 歩 歩 歩 歩 歩 歩|七
+| ・ 角 ・ ・ ・ ・ ・ 飛 ・|八
+| ・ 桂 銀 金 玉 金 銀 桂 香|九
++---------------------------+
+EOT
+
+info2 = TeaiInfo["香落ち"].both_board_info
+info1[L.b] == info2[L.b]        # => true
+
+TeaiInfo.find {|e| e.both_board_info[L.b] == info1[L.b] }.name # => "香落ち"
+
