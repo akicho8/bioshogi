f519b1c7cb8b2c9aa54be26ff66538d4c1aa9c30
akicho8 <akicho8@gmail.com>
2017-11-18 18:58:34 +0900

    ROOK や rook で駒にマッチできる機能はまったく使ってない→削除

--- lib/bushido/piece.rb
+++ lib/bushido/piece.rb
@@ -145,13 +145,13 @@ module Bushido
 
       # 成ってないときの名前たち
       def basic_names
-        [name, key.to_s, basic_alias].flatten.compact
+        [name, basic_alias, csa_name1].flatten.compact
       end
 
       # 成ったときの名前たち
       # 「キーの大文字」を成名としているのはおまけ
       def promoted_names
-        [promoted_name, key.to_s.upcase, promoted_alias].flatten.compact
+        [promoted_name, promoted_alias, csa_name2].flatten.compact
       end
     end
 
