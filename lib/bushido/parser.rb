ab080b4108ef0f3fda90f0d4efca8e567617bd98
akicho8 <akicho8@gmail.com>
2017-11-21 09:01:02 +0900

    Parserモジュールの得意メソッドがなんで parser/base にある？→移動

--- lib/bushido/parser.rb
+++ lib/bushido/parser.rb
@@ -1,4 +1,35 @@
-require_relative "parser/base"
-require_relative "parser/csa_parser"
-require_relative "parser/kif_parser"
-require_relative "parser/ki2_parser"
+module Bushido
+  module Parser
+    extend self
+
+    # 棋譜ファイルのコンテンツを読み込む
+    def parse(source, **options)
+      parser = support_parsers.find { |e| e.accept?(source) }
+      unless parser
+        raise FileFormatError, "棋譜のフォーマットが不明です : #{source}"
+      end
+      parser.parse(source, options)
+    end
+
+    # 棋譜ファイル自体を読み込む
+    def parse_file(file, **options)
+      parse(Pathname(file).expand_path.read, options)
+    end
+
+    # source が Pathname ならそのファイルから読み込み、文字列なら何もしない
+    # こういう設計はやりすぎな感もあるけど open-uri で open が URL にも対応する前例があるのでOK
+    def source_normalize(source)
+      if source.kind_of?(Pathname)
+        source = source.expand_path.read
+      end
+      s = source.to_s.toutf8 + "\n" # 最後の行にも必ず改行で終わるようにする
+      s = s.gsub(/\p{blank}*\R/, "\n")
+    end
+
+    private
+
+    def support_parsers
+      [KifParser, CsaParser, Ki2Parser]
+    end
+  end
+end
--- lib/bushido/parser/base.rb
+++ lib/bushido/parser/base.rb
@@ -9,37 +9,6 @@ require_relative "header_info"
 
 module Bushido
   module Parser
-    class << self
-      # 棋譜ファイルのコンテンツを読み込む
-      def parse(source, **options)
-        parser = support_parsers.find { |e| e.accept?(source) }
-        parser or raise FileFormatError, "棋譜のフォーマットが不明です : #{source}"
-        parser.parse(source, options)
-      end
-
-      # 棋譜ファイル自体を読み込む
-      def parse_file(file, **options)
-        parse(Pathname(file).expand_path.read, options)
-      end
-
-      # source が Pathname ならそのファイルから読み込み、文字列なら何もしない
-      #   こういう設計はいまいちな感もあるけど open-uri で open がURLからも読み込むようになるのに似ているからいいとする
-      def source_normalize(source)
-        if source.kind_of?(Pathname)
-          source = source.expand_path.read
-        end
-        s = source.to_s.toutf8 + "\n" # 最後の行にも必ず改行で終わるようにする
-        s = s.gsub(/\p{blank}*\R/, "\n")
-      end
-
-      private
-
-      def support_parsers
-        [KifParser, CsaParser, Ki2Parser]
-      end
-    end
-
-    # kif/ki2読み込みの共通処理
     class Base
       cattr_accessor(:header_sep) { "：" }
 
