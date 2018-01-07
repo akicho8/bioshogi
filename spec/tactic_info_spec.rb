c3d89ad0cfd023c5d52d35d5f0b1e2976bfa36fb
akicho8 <akicho8@gmail.com>
2018-01-07 19:33:33 +0900

    specの方に全戦法の判定テストを追加

---
+++ spec/tactic_info_spec.rb
@@ -0,0 +1,17 @@
+require_relative "spec_helper"
+
+module Bushido
+  describe TacticInfo do
+    it "すべての戦法の判定" do
+      TacticInfo.all_elements.each do |e|
+        file = Pathname.glob("#{__dir__}/../experiment/#{e.tactic_info.name}/#{e.key}.*").first
+        info = Parser.file_parse(file)
+        info.mediator_run
+        matches = info.mediator.players.collect { |player|
+          player.skill_set.public_send("normalized_#{e.tactic_info.key}_infos").collect(&:key)
+        }.flatten
+        assert matches.include?(e.key), e.key
+      end
+    end
+  end
+end
