489a3facdf6284cb92109f378619c672bb4b1704
akicho8 <akicho8@gmail.com>
2018-03-16 19:51:19 +0900

    [chore] Update experiment

--- experiment/00120_piece.rb
+++ experiment/00120_piece.rb
@@ -8,12 +8,14 @@ Piece.s_to_h2("▲歩2 飛 △歩二飛 ▲金")               # => {:black=>{:p
 Piece.h_to_s(bishop: 1, rook: 2)                      # => "飛二 角"
 
 tp Piece["飛"].to_h
-# >> |----------------+--------------------------------------------|
-# >> |            key | rook                                       |
-# >> |           name | 飛                                         |
-# >> |  promoted_name | 龍                                         |
-# >> |    basic_names | ["飛", "HI", "R", :rook]                   |
-# >> | promoted_names | ["龍", "竜", "RY"]                         |
-# >> |          names | ["飛", "HI", "R", :rook, "龍", "竜", "RY"] |
-# >> |     promotable | true                                       |
-# >> |----------------+--------------------------------------------|
+# >> |----------------+------|
+# >> |            key | rook |
+# >> |           name | 飛   |
+# >> |    basic_alias |      |
+# >> |  promoted_name | 龍   |
+# >> | promoted_alias | 竜   |
+# >> |      sfen_char | R    |
+# >> |     promotable | true |
+# >> |   always_alive | true |
+# >> |           code | 1    |
+# >> |----------------+------|
--- experiment/00160_piece_box.rb
+++ experiment/00160_piece_box.rb
@@ -19,7 +19,7 @@ piece_box.to_h                                 # => {:king=>1, :bishop=>3}
 piece_box.to_sfen(:black)                      # => "K3B"
 piece_box.to_csa(:black)                       # => "P+00OU00KA00KA00KA"
 piece_box.to_s(separator: "/")                 # => "玉/角三"
-piece_box.score                                # => 15669
+piece_box.score                                # => 45670
 
 a = PieceBox.new(Piece.s_to_h("飛玉"))
 b = PieceBox.new(Piece.s_to_h("玉飛"))
