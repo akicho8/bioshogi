c2b9dadfd70b8d9bfe5bae7d3e06bf2f8247dfcd
akicho8 <akicho8@gmail.com>
2018-04-20 10:31:24 +0900

    [chore] 千日手チェックは未完成なので中途半端に実装した重い処理を除外

--- lib/warabi/player_executor_cpu.rb
+++ lib/warabi/player_executor_cpu.rb
@@ -3,7 +3,7 @@
 module Warabi
   class PlayerExecutorCpu < PlayerExecutorHuman
     def turn_changed_process
-      mediator.one_place_map[mediator.one_place_hash] += 1
+      # mediator.one_place_map[mediator.one_place_hash] += 1
     end
   end
 end
