a20e7f4937132a52054f1d114c1005728f0f70d8
akicho8 <akicho8@gmail.com>
2018-04-28 19:34:13 +0900

    [refactor] saferenum experiment

--- experiment/00130_place.rb
+++ experiment/00130_place.rb
@@ -9,20 +9,20 @@ b = Place["２一"]
 [a, b].sort rescue $!           # => [#<Warabi::Place ２一>, #<Warabi::Place １一>]
 [a, b].sort_by(&:to_xy)         # => [#<Warabi::Place ２一>, #<Warabi::Place １一>]
 
-Place["１一"].object_id         # => 70116898262600
-Place["11"].object_id           # => 70116898262600
+Place["１一"].object_id         # => 70337763137220
+Place["11"].object_id           # => 70337763137220
 
 # Dimension::Yplace.fetch(1).object_id # => 70357212614280
 # Dimension::Yplace.fetch(1).object_id # => 70357212614280
 
-Place["76"].hash                # => -4488517606073517175
-Place["７６"].hash              # => -4488517606073517175
-Place["76"].object_id           # => 70116893290840
-Place["７６"].object_id         # => 70116893290840
+Place["76"].hash                # => -3969640160796472671
+Place["７６"].hash              # => -3969640160796472671
+Place["76"].object_id           # => 70337763121860
+Place["７６"].object_id         # => 70337763121860
 hash = {}
 
 hash[Place["76"]] = 1
 hash[Place["７６"]]             # => 1
 
-[1, 2].hash                     # => 3840442060567125757
-[1, 2].hash                     # => 3840442060567125757
+[1, 2].hash                     # => -3776967920834308657
+[1, 2].hash                     # => -3776967920834308657
