1f78f84560284db5d3c800567fd0570fa8ddffa2
akicho8 <akicho8@gmail.com>
2018-06-28 11:33:50 +0900

    [chore] add kifu_format_info

---
+++ lib/warabi/kifu_format_info.rb
@@ -0,0 +1,15 @@
+module Warabi
+  class KifuFormatInfo
+    include ApplicationMemoryRecord
+    memory_record [
+      { key: "ki2",  },
+      { key: "kif",  },
+      { key: "csa",  },
+      { key: "sfen", },
+    ]
+
+    def name
+      @name ||= key.to_s.upcase
+    end
+  end
+end
