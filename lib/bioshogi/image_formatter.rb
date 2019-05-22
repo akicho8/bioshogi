c0769620547195c53600f428fe6cbca9e307e800
akicho8 <akicho8@gmail.com>
2019-05-22 15:46:27 +0900

    [feat] image_formatter のフォント固定化

--- lib/bioshogi/image_formatter.rb
+++ lib/bioshogi/image_formatter.rb
@@ -28,25 +28,28 @@ module Bioshogi
   class ImageFormatter
     cattr_accessor :default_params do
       {
+        # required
         width: 1600,                   # 画像横幅
         height: 630,                   # 画像縦幅
         board_rate: 0.96,              # 縦横幅の小さい方に対する盤の寸法の割合
-        # normal_font: "Meiryo",         # 駒のフォント
-        normal_font: "/Users/ikeda/Library/Fonts/Ricty-Regular.ttf",
-        bold_font: "MeiryoB",          # 駒のフォント(ボールド)
         font_size: 0.6,                # 文字の大きさの割合(割合はすべてセルの大きさを1.0とする)
         stand_piece_line_height: 0.95, # 持駒と持駒の間隔割合
         stand_piece_count_gap: 0.6,    # 持駒と駒数の間隔の割合
         piece_pull_rate: 0.1,          # 盤上の位置を下げる割合
         star_size: 0.03,               # 星のサイズ(割合)
-        paper_color: "white",          # 下地の色
-        piece_name_color: "black",     # 駒の色
-        piece_count_color: "           #888",     # 駒数の色
-        lattice_color: "               #888",         # 格子の色
-        promoted_color: "red",         # 成駒の色
+        canvas_color: "white",         # 下地の色(必須)
+        piece_color: "black",          # 駒の色(必須)
         lattice_stroke_width: 1,       # 格子の線のドット数
-        frame_stroke_width: 2,         # 格子の外枠の線のドット数
-        moving_color: "                #f0f0f0",       # 移動元と移動先のセルの背景色(falseなら描画しない)
+
+        normal_font: "#{__dir__}/RictyDiminished-Regular.ttf", # 駒のフォント(普通)
+        bold_font: "#{__dir__}/RictyDiminished-Bold.ttf",      # 駒のフォント(太字) (nilなら normal_font を代用)
+
+        # optional
+        piece_count_color: "#888",     # *駒数の色(nilなら piece_color を代用)
+        lattice_color: "#888",         # *格子の色(nilなら piece_color を代用)
+        promoted_color: "red",         # *成駒の色(nilなら piece_color を代用)
+        frame_stroke_width: 2,         # 格子の外枠の線のドット数(nil なら lattice_stroke_width を代用)
+        moving_color: "#f0f0f0",       # 移動元と移動先のセルの背景色(nilなら描画ない)
       }
     end
 
@@ -97,44 +100,66 @@ module Bioshogi
     end
 
     def canvas
-      @canvas ||= Magick::ImageList.new.tap do |canvas|
-        canvas.new_image(*image_rect).tap do |e|
-          e.background_color = params[:paper_color]
+      @canvas ||= -> {
+        # canvas = Magick::Image.new(*image_rect)
+        # if params[:canvas_color]
+        #   canvas.background_color = params[:canvas_color]
+        # end
+        # canvas.background_color = "blue"
+        # canvas
+
+        # https://github.com/rmagick/rmagick/issues/699
+        canvas = Magick::ImageList.new
+        params = self.params   # FIXME: new_image のブロック内スコープが Image.new のなかなので params が参照できないため
+        canvas.new_image(*image_rect) do |e|
+          e.background_color = params[:canvas_color]
         end
-      end
+        canvas
+
+        # list = Magick::ImageList.new
+        # list.new_image(*image_rect)
+        # list.last.background_color = "red" # params[:canvas_color]
+        # list
+      }.call
     end
 
     private
 
+    def lattice_color
+      params[:lattice_color] || params[:piece_color]
+    end
+
     def lattice_draw
       draw_context do |c|
-        c.stroke(params[:lattice_color])
-        c.stroke_width(params[:lattice_stroke_width])
+        c.stroke(lattice_color)
+        c.stroke_width(lattice_stroke_width)
 
-        lattice.w.next.times do |x|
+        (1...lattice.w).each do |x|
           line_draw(c, V[x, 0], V[x, lattice.h])
         end
 
-        lattice.h.next.times do |y|
+        (1...lattice.h).each do |y|
           line_draw(c, V[0, y], V[lattice.w, y])
         end
       end
     end
 
     def frame_draw
-      draw_context do |c|
-        c.stroke(params[:lattice_color])
-        c.stroke_width(params[:frame_stroke_width])
-        c.stroke_linejoin("round") # 曲がり角を丸める
-        c.fill = "transparent"
-        c.rectangle(*px(V[0, 0]), *px(V[lattice.w, lattice.h]))
+      if frame_stroke_width
+        draw_context do |c|
+          c.stroke(lattice_color)
+          c.stroke_width(frame_stroke_width)
+          c.stroke_linejoin("round") # 曲がり角を丸める
+          c.fill = "transparent"
+          c.rectangle(*px(V[0, 0]), *px(V[lattice.w, lattice.h]))
+        end
       end
     end
 
     def star_draw
       draw_context do |c|
         c.stroke("transparent")
-        c.fill(params[:lattice_color])
+        c.fill(lattice_color)
 
         i = star_step
         (i...lattice.w).step(i) do |x|
@@ -165,9 +190,9 @@ module Bioshogi
           v = V[x, y]
           if soldier = parser.mediator.board.lookup(v)
             if soldier.promoted
-              color = params[:promoted_color]
+              color = params[:promoted_color] || params[:piece_color]
             else
-              color = params[:piece_name_color]
+              color = params[:piece_color]
             end
             bold = hand_log && soldier == hand_log.soldier
             v2 = v + V[0, 1] * params[:piece_pull_rate] * soldier.location.value_sign
@@ -197,18 +222,18 @@ module Bioshogi
       end
     end
 
-    def char_draw(pos:, text:, rotation:, color: params[:piece_name_color], bold: false)
+    def char_draw(pos:, text:, rotation:, color: params[:piece_color], bold: false)
       c = Magick::Draw.new
       c.rotation = rotation
+      # c.font_weight = Magick::BoldWeight # 効かない
       c.pointsize = real_pointsize
       if bold
-        c.font = params[:bold_font]
+        c.font = params[:bold_font] || params[:normal_font]
       else
         c.font = params[:normal_font]
       end
       c.stroke = "transparent"
-      # c.stroke_width = 2
-      # c.stroke_antialias(true)
+      # c.stroke_antialias(false) # 効かない？
       c.fill = color
       c.gravity = Magick::CenterGravity
       c.annotate(canvas, *cell_size_rect, *px(pos), text)
@@ -238,7 +263,7 @@ module Bioshogi
           char_draw(pos: v, text: piece.name, rotation: location.angle)  # 駒
           if count > 1
             pos = v + V[params[:stand_piece_count_gap], 0] * s
-            char_draw(pos: pos, text: count.to_s, rotation: location.angle, color: params[:piece_count_color]) # 駒数
+            char_draw(pos: pos, text: count.to_s, rotation: location.angle, color: params[:piece_count_color] || params[:piece_color]) # 駒数
           end
           v += V[0, 1] * g * s
         end
@@ -302,6 +327,14 @@ module Bioshogi
 
     def v_top_left_outer
       @v_top_left_outer ||= V[-1, 0]
+    end
+
+    def lattice_stroke_width
+      params[:lattice_stroke_width]
+    end
+
+    def frame_stroke_width
+      params[:frame_stroke_width] || lattice_stroke_width
     end
 
     class V < Vector
