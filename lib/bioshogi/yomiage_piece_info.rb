1ffd3d5333f9a56b8673764ba2fb9b6948b1dc1b
akicho8 <akicho8@gmail.com>
2019-05-23 17:23:22 +0900

    [fix] 「金」を「かね」と発音してしまう

--- lib/bioshogi/yomiage_piece_info.rb
+++ lib/bioshogi/yomiage_piece_info.rb
@@ -3,10 +3,10 @@ module Bioshogi
     include ApplicationMemoryRecord
     memory_record [
       { key: :king,   name: "ぎょく", promoted_name: nil,          },
-      { key: :rook,   name: "ひしゃ", promoted_name: "龍",         }, # 「飛車」だと発音がおかしいため
+      { key: :rook,   name: "ひしゃ", promoted_name: "りゅう",     }, # 「飛車」だと発音がおかしいため
       { key: :bishop, name: "かく",   promoted_name: "うま",       }, # 「角」は「かど」、「馬」は「ば」と読まれてしまうため
-      { key: :gold,   name: "金",     promoted_name: nil,          },
-      { key: :silver, name: "ぎん",   promoted_name: "成り銀",     },
+      { key: :gold,   name: "きん",   promoted_name: nil,          },
+      { key: :silver, name: "ぎん",   promoted_name: "なりぎん",   },
       { key: :knight, name: "けいま", promoted_name: "なりけい",   },
       { key: :lance,  name: "きょう", promoted_name: "なりきょう", },
       { key: :pawn,   name: "ふ",     promoted_name: "と",         },
