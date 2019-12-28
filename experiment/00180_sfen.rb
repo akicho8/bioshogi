64a45929e20198e8c8990245d4b26ffeaea1bfbf
akicho8 <akicho8@gmail.com>
2019-12-28 21:09:00 +0900

    [refactor] experiment/*

--- experiment/00180_sfen.rb
+++ experiment/00180_sfen.rb
@@ -7,23 +7,8 @@ sfen.move_infos                        # => [{:input=>"7i6h"}, {:input=>"S*2d"}]
 sfen.piece_counts                      # => {:black=>{:silver=>1}, :white=>{:silver=>2}}
 sfen.turn_counter                      # => 0
 sfen.handicap?                         # => false
-tp sfen.attributes
+sfen.attributes                        # => {:board=>"lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL", :b_or_w=>"b", :hold_pieces=>"S2s", :turn_counter_next=>"1", :moves=>"7i6h S*2d"}
 
 sfen = Sfen.parse("position startpos")
-tp sfen.attributes
-sfen.soldiers
-
-# >> |-------------------+-----------------------------------------------------------|
-# >> |             board | lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL |
-# >> |            b_or_w | b                                                         |
-# >> |       hold_pieces | S2s                                                       |
-# >> | turn_counter_next | 1                                                         |
-# >> |             moves | 7i6h S*2d                                                 |
-# >> |-------------------+-----------------------------------------------------------|
-# >> |-------------------+-----------------------------------------------------------|
-# >> |             board | lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL |
-# >> |            b_or_w | b                                                         |
-# >> |       hold_pieces | -                                                         |
-# >> | turn_counter_next | 1                                                         |
-# >> |             moves |                                                           |
-# >> |-------------------+-----------------------------------------------------------|
+sfen.attributes # => {:board=>"lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL", :b_or_w=>"b", :hold_pieces=>"-", :turn_counter_next=>"1", :moves=>nil}
+sfen.soldiers.collect(&:to_s)
