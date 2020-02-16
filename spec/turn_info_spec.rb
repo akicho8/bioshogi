0cc8cd8130e2f054e6a8cd32e711ed5480965eac
akicho8 <akicho8@gmail.com>
2020-02-16 14:58:20 +0900

    [refactor] turn_info_spec

--- spec/turn_info_spec.rb
+++ spec/turn_info_spec.rb
@@ -2,24 +2,24 @@ require_relative "spec_helper"
 
 module Bioshogi
   describe TurnInfo do
-    it "駒落ちなので△から始まる" do
-      turn_info = TurnInfo.new(handicap: true)
+    it "1手づつ先後が変化する" do
+      turn_info = TurnInfo.new
+      assert { turn_info.current_location.key == :black }
+      turn_info.turn_offset += 1
       assert { turn_info.current_location.key == :white }
     end
 
-    it "平手なので▲から始まる" do
-      turn_info = TurnInfo.new
-      assert { turn_info.current_location.key == :black }
+    it "駒落ちなら△から始まる" do
+      assert { TurnInfo.new(handicap: false).current_location.key == :black }
+      assert { TurnInfo.new(handicap: true).current_location.key == :white  }
     end
 
-    it "display_turn" do
-      turn_info = TurnInfo.new
-      turn_info.turn_base = 2
-      turn_info.turn_offset = 3
-      assert { turn_info.display_turn == 5 }
+    it "内部で何手目か始まったを持っているのでオフセット2でも表示は変わる。また駒落ちかどうかには関係がない" do
+      assert { TurnInfo.new(handicap: false, turn_base: 2, turn_offset: 3).display_turn == 5 }
+      assert { TurnInfo.new(handicap: true,  turn_base: 2, turn_offset: 3).display_turn == 5 }
     end
 
-    it "turn_offset_zero_location" do
+    it "1手も動かしてない状態の手番を返す(turn_offsetは無視する)" do
       assert { TurnInfo.new(handicap: true,  turn_base: 0, turn_offset: 1).turn_offset_zero_location.key == :white }
       assert { TurnInfo.new(handicap: false, turn_base: 0, turn_offset: 1).turn_offset_zero_location.key == :black }
       assert { TurnInfo.new(handicap: true,  turn_base: 1, turn_offset: 1).turn_offset_zero_location.key == :black }
