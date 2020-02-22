d0b36e5c0f5f7ffc569cbd7eac32fc0a94170a73
akicho8 <akicho8@gmail.com>
2020-02-22 23:01:47 +0900

    [chore] ミクロコスモスのテスト追加

---
+++ spec/microcosmos_spec.rb
@@ -0,0 +1,31 @@
+require_relative "spec_helper"
+require "active_support/core_ext/benchmark"
+
+module Bioshogi
+  describe "ミクロコスモス" do
+    it do
+      bod = nil
+      ms = Benchmark.ms { bod = Parser.file_parse(Pathname(__dir__).join("../experiment/microcosmos.kif")).to_bod }
+      assert { ms < 1130 }
+      assert { bod == <<~EOT }
+後手の持駒：飛二 角 銀 桂二 香 歩三
+  ９ ８ ７ ６ ５ ４ ３ ２ １
++---------------------------+
+| ・ ・ ・ と と と と 杏 ・|一
+| ・v金 ・ ・ ・ ・ ・ ・ ・|二
+| ・v桂 ・ ・v歩v歩v歩v歩 ・|三
+| ・ ・ とv銀v金vと ・ ・ ・|四
+|v馬v圭 香 ・ ・ ・ ・ 銀 ・|五
+| ・ 歩 歩 ・ ・ ・ と ・ 香|六
+| ・ ・ ・ ・ ・ ・ ・ ・ ・|七
+| ・ ・ ・ ・ とv銀 歩 ・ ・|八
+| ・ ・ ・ ・ ・ ・ 金 金v玉|九
++---------------------------+
+先手の持駒：なし
+手数＝1525 ▲２九金打 まで
+
+後手番
+EOT
+    end
+  end
+end
