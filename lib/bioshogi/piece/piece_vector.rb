3128e4d118c083972c081ced555be63288a215d5
akicho8 <akicho8@gmail.com>
2020-07-29 12:05:35 +0900

    [refactor] PieceVector

--- lib/bioshogi/piece/piece_vector.rb
+++ lib/bioshogi/piece/piece_vector.rb
@@ -15,9 +15,9 @@ module Bioshogi
         { key: :bishop, basic_once_vectors: nil,                 basic_repeat_vectors: :pattern_x,    promoted_once_vectors: :pattern_plus, promoted_repeat_vectors: :pattern_x,    },
         { key: :gold,   basic_once_vectors: :pattern_gold,       basic_repeat_vectors: nil,           promoted_once_vectors: nil,           promoted_repeat_vectors: nil,           },
         { key: :silver, basic_once_vectors: :pattern_silver,     basic_repeat_vectors: nil,           promoted_once_vectors: :pattern_gold, promoted_repeat_vectors: nil,           },
-        { key: :knight, basic_once_vectors: [[-1, -2], [1, -2]], basic_repeat_vectors: nil,           promoted_once_vectors: :pattern_gold, promoted_repeat_vectors: nil,           },
-        { key: :lance,  basic_once_vectors: nil,                 basic_repeat_vectors: [[0, -1]],     promoted_once_vectors: :pattern_gold, promoted_repeat_vectors: nil,           },
-        { key: :pawn,   basic_once_vectors: [[0, -1]],           basic_repeat_vectors: nil,           promoted_once_vectors: :pattern_gold, promoted_repeat_vectors: nil,           },
+        { key: :knight, basic_once_vectors: :pattern_knight,     basic_repeat_vectors: nil,           promoted_once_vectors: :pattern_gold, promoted_repeat_vectors: nil,           },
+        { key: :lance,  basic_once_vectors: nil,                 basic_repeat_vectors: :pattern_pawn, promoted_once_vectors: :pattern_gold, promoted_repeat_vectors: nil,           },
+        { key: :pawn,   basic_once_vectors: :pattern_pawn,       basic_repeat_vectors: nil,           promoted_once_vectors: :pattern_gold, promoted_repeat_vectors: nil,           },
       ]
 
       def brave?
@@ -27,14 +27,14 @@ module Bioshogi
       def all_vectors(promoted:, location:)
         @all_vectors ||= {}
         @all_vectors[[promoted, location.key]] ||= -> {
-          vectors = __select_vectors(promoted)
+          vectors = select_vectors(promoted)
           normalized_vectors(location, vectors)
         }.call
       end
 
       private
 
-      def __select_vectors(promoted)
+      def select_vectors(promoted)
         raise MustNotHappen if !piece.promotable? && promoted
 
         if promoted
@@ -76,28 +76,24 @@ module Bioshogi
       end
 
       def basic_once_vectors
-        __vectors_at(super)
+        vectors_at(super)
       end
 
       def basic_repeat_vectors
-        __vectors_at(super)
+        vectors_at(super)
       end
 
       def promoted_once_vectors
-        __vectors_at(super)
+        vectors_at(super)
       end
 
       def promoted_repeat_vectors
-        __vectors_at(super)
+        vectors_at(super)
       end
 
-      def __vectors_at(value)
+      def vectors_at(value)
         if value
-          if value.kind_of?(Symbol)
-            send(value)
-          else
-            value
-          end
+          send(value)
         else
           []
         end
@@ -105,41 +101,57 @@ module Bioshogi
 
       def pattern_plus
         [
-          nil,    [0,-1],   nil,
-          [-1, 0],       [1, 0],
-          nil,    [0, 1],   nil,
+          nil,     [0,-1],     nil,
+          [-1, 0],    nil, [+1, 0],
+          nil,     [0,+1],     nil,
         ]
       end
 
       def pattern_x
         [
-          [-1, -1], nil, [1, -1],
+          [-1, -1], nil, [+1, -1],
           nil,      nil,     nil,
-          [-1,  1], nil, [1,  1],
+          [-1, +1], nil, [+1, +1],
         ]
       end
 
       def pattern_silver
         [
-          [-1, -1], [0, -1], [1, -1],
-          nil,          nil,     nil,
-          [-1,  1],     nil, [1,  1],
+          [-1, -1], [0, -1], [+1, -1],
+          nil,          nil,      nil,
+          [-1, +1],     nil, [+1, +1],
         ]
       end
 
       def pattern_gold
         [
-          [-1, -1], [0, -1], [1, -1],
-          [-1,  0],          [1,  0],
-          nil,      [0,  1],     nil,
+          [-1, -1], [0, -1], [+1, -1],
+          [-1,  0],          [+1,  0],
+          nil,      [0, +1],      nil,
         ]
       end
 
       def pattern_king
         [
-          [-1, -1], [0, -1], [1, -1],
-          [-1,  0],     nil, [1,  0],
-          [-1,  1], [0,  1], [1,  1],
+          [-1, -1], [0, -1], [+1, -1],
+          [-1,  0],     nil, [+1,  0],
+          [-1, +1], [0, +1], [+1, +1],
+        ]
+      end
+
+      def pattern_knight
+        [
+          [-1, -2], nil, [+1, -2],
+          nil,      nil,      nil,
+          nil,      nil,      nil,
+        ]
+      end
+
+      def pattern_pawn
+        [
+          nil, [0, -1], nil,
+          nil,     nil, nil,
+          nil,     nil, nil,
         ]
       end
     end
