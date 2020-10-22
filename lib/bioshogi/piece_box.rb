cb54f674990b0ca0b4b72fbdd0a9bc077e1a167e
akicho8 <akicho8@gmail.com>
2020-10-22 17:11:01 +0900

    [chore] エラー文言改善

--- lib/bioshogi/piece_box.rb
+++ lib/bioshogi/piece_box.rb
@@ -20,7 +20,7 @@ module Bioshogi
     def pick_out(piece)
       piece = Piece.fetch(piece)
       unless exist?(piece)
-        raise HoldPieceNotFound, "#{piece.name}を持っていません : #{to_h}"
+        raise HoldPieceNotFound, "持駒から#{piece.name}を取り出そうとしましたが#{piece.name}を持っていません : #{to_h}"
       end
       add(piece => -1)
       piece
