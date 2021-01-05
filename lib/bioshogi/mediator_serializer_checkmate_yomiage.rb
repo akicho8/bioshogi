b107e95f08b87a79695c41fe8a22b1fa42c086e6
akicho8 <akicho8@gmail.com>
2021-01-05 16:07:27 +0900

    [feat] 持駒なしの場合を考慮

--- lib/bioshogi/mediator_serializer_checkmate_yomiage.rb
+++ lib/bioshogi/mediator_serializer_checkmate_yomiage.rb
@@ -37,7 +37,7 @@ module Bioshogi
     def piece_box
       piece_box = mediator.player_at(:black).piece_box
       piece_counts = piece_box.sort_by { |e, count| -Piece.fetch(e).basic_weight }
-      "もちごま。" + piece_counts.collect { |e, count| (Piece.fetch(e).yomiage(false) + "。") * count }.join # =>
+      "もちごま。" + piece_counts.collect { |e, count| (Piece.fetch(e).yomiage(false) + "。") * count }.join.presence || "なし。"
     end
   end
 end
