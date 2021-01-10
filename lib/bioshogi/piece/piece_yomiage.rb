f35f999fc24e630a74e3f8421c4efcc083481acc
akicho8 <akicho8@gmail.com>
2021-01-11 08:11:14 +0900

    [fix] 「なりgin」は「なりじん」と読まれてしまう

--- lib/bioshogi/piece/piece_yomiage.rb
+++ lib/bioshogi/piece/piece_yomiage.rb
@@ -9,7 +9,7 @@ module Bioshogi
         { key: :rook,   name: "hisha",  promoted_name: "ryu",       }, # 「飛車」だと発音がおかしいため
         { key: :bishop, name: "kaku",   promoted_name: "うま",      }, # 「角」は「かど」、「馬」は「ば」と読まれてしまうため
         { key: :gold,   name: "kin",    promoted_name: nil,         },
-        { key: :silver, name: "gin",    promoted_name: "なりgin",   },
+        { key: :silver, name: "gin",    promoted_name: "なり銀",    }, # 「なりgin」は「なりじん」と読まれてしまう
         { key: :knight, name: "keima",  promoted_name: "なりkei",   },
         { key: :lance,  name: "kyo",    promoted_name: "なりkyo",   },
         { key: :pawn,   name: "hu",     promoted_name: "「と」",    },
