da7a4f37272460ae1aa5687e6bc0a4aa401ea10f
akicho8 <akicho8@gmail.com>
2021-08-03 20:32:22 +0900

    [fix] animation_format --> animation_formatter

--- lib/bioshogi/formatter/binary_format_methods.rb
+++ lib/bioshogi/formatter/binary_format_methods.rb
@@ -9,12 +9,12 @@ module Bioshogi
         image_formatter(options).to_blob
       end
 
-      def animation_format(options = {})
+      def animation_formatter(options = {})
         AnimationFormatter.render(self, options)
       end
 
       def to_animation(options = {})
-        animation_format(options).to_blob
+        animation_formatter(options).to_blob
       end
     end
   end
