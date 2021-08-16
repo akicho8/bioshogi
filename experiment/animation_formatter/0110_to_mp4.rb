cb78d651f34d6437a7e54f1b1b3624028984a4f5
akicho8 <akicho8@gmail.com>
2021-08-16 21:32:16 +0900

    [feat] Audio 付き to_mp4

--- experiment/animation_formatter/0110_to_mp4.rb
+++ experiment/animation_formatter/0110_to_mp4.rb
@@ -1,6 +1,11 @@
 require "../example_helper"
 
 info = Parser.parse("position sfen lnsgkgsnl/1r7/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL w - 1 moves 8c8d 7g7f")
-bin = info.to_mp4(video_speed: 0.5)
-Pathname("_output.mp4").write(bin) # => 37987
-`open _output.mp4`
+bin = info.to_mp4(video_speed: 5.0)
+Pathname("_output.mp4").write(bin) # => 279271
+`open -a 'google chrome' _output.mp4`
+# >> /var/folders/9c/_62dfc8502g_d5r05zyfwlxh0000gn/T/d20210816-2073-fuao7d
+# >> ffmpeg -v warning -hide_banner -r 1000/5000 -y -i _output0.mp4 -c:v libx264 -pix_fmt yuv420p -y _output1_yuv420.mp4
+# >> ffmpeg -v warning -stream_loop -1 -i /Users/ikeda/src/bioshogi/lib/bioshogi/loop_bgm.m4a -t 15.0 -af "afade=t=out:st=10.0:d=5" -y _fade.m4a
+# >> 15.0
+# >> ffmpeg -v warning -i _output1_yuv420.mp4 -i _fade.m4a -c copy -y _output2.mp4
--- lib/bioshogi/mp4_formatter.rb
+++ lib/bioshogi/mp4_formatter.rb
@@ -1,3 +1,10 @@
+# ruby 0102_speed_compare.rb
+# 7819.15000002482
+# 9034.157999994932
+# -rw------- 1 ikeda staff  60K  8 16 19:16 _output0.mp4
+# -rw-r--r-- 1 ikeda staff 119K  8 16 19:16 _output1_1.mp4
+# -rw-r--r-- 1 ikeda staff 135K  8 16 19:17 _output1_2.mp4
+
 require "systemu"
 
 module Bioshogi
@@ -20,51 +27,65 @@ module Bioshogi
     end
 
     def to_binary
-      # animation_formatter は使わずにここで書けばいいんじゃないか？
-      # e = @parser.animation_formatter(params.merge(animation_format: "mp4", delay_per_one: 0))
-      # bin = e.to_write_binary
-      bin = rmagick_mp4
-      in_out_tempfile do |i, o|
-        i.write(bin)
-        command = ffmpeg_command(i, o)
-        strict_system(command)
+      require "rmagick"
+      begin
+        dir = Dir.mktmpdir
+        puts dir
+
+        Dir.chdir(dir) do
+          mediator = parser.mediator_for_image
+          image_formatter = ImageFormatter.new(mediator, params)
+
+          @list = Magick::ImageList.new
+          image_formatter.render
+          @list.concat([image_formatter.canvas])
+          parser.move_infos.each do |e|
+            mediator.execute(e[:input])
+            image_formatter.render
+            @list.concat([image_formatter.canvas]) # canvas は Magick::Image のインスタンス
+          end
+          @list.concat([image_formatter.canvas] * params[:end_frames])
+          frame_count = @list.count
+
+          @list.write("_output0.mp4")
+          @list.destroy!
+
+          # puts `duration _output1.mp4`
+
+          strict_system %(ffmpeg -v warning -hide_banner #{fps_option} -y -i _output0.mp4 -c:v libx264 -pix_fmt yuv420p -y _output1_yuv420.mp4)
+
+          loop_bgm = Pathname("#{__dir__}/loop_bgm.m4a")
+
+          #   # # 5秒の素材を繰り返して8秒(正確に8秒ではない)のBGMを作る
+          total = frame_count * video_speed
+          fadein = 0
+          fadeout = 5
+          st = total - fadeout
+          # 
+          # strict_system %(ffmpeg -v warning -stream_loop -1 -i #{loop_bgm} -t #{total} -c copy -y _fade.m4a)
+          # afade=t=in:st=0:d=#{fadeout},
+          strict_system %(ffmpeg -v warning -stream_loop -1 -i #{loop_bgm} -t #{total} -af "afade=t=out:st=#{st}:d=#{fadeout}" -y _fade.m4a)
+          # puts `duration loop_bgm.m4a`         # => "3.018594\n"
+          puts `duration _fade.m4a`            # => "8.0\n"
+
+          # # # 前後をフェイドアウトさせる(処理は↑と一体化できる)
+          # # ffmpeg -v warning -i _long.m4a -af "afade=t=in:st=0:d=1,afade=t=out:st=6:d=2" -y _fade.m4a
+          #
+          # # # 結合する
+          strict_system %(ffmpeg -v warning -i _output1_yuv420.mp4 -i _fade.m4a -c copy -y _output2.mp4)
+          # `open -a 'google chrome' _output2.mp4`
+
+          Pathname("_output2.mp4").read
+        end
+      ensure
+        FileUtils.remove_entry_secure(dir)
       end
     end
 
     private
 
-    def rmagick_mp4
-      require "rmagick"
-
-      mediator = parser.mediator_for_image
-      image_formatter = ImageFormatter.new(mediator, params)
-
-      @list = Magick::ImageList.new
-      image_formatter.render
-      @list.concat([image_formatter.canvas])
-      parser.move_infos.each do |e|
-        mediator.execute(e[:input])
-        image_formatter.render
-        @list.concat([image_formatter.canvas]) # canvas は Magick::Image のインスタンス
-      end
-      @list.concat([image_formatter.canvas] * params[:end_frames])
-      # @list.delay = @list.ticks_per_second * params[:delay_per_one]
-      # @list = @list.coalesce                               # 全ページを1ページ目のフレームサイズにする
-      # if params[:optimize_layer]
-      @list = @list.optimize_layers(Magick::OptimizeLayer) # 各ページを最小枠にする
-      #
-      # @list.iterations = iterations_number  # 繰り返し回数
-
-      Tempfile.create(["", ".mp4"]) do |t|
-        @list.write(t.path)
-        @list.destroy!
-        File.read(t.path)
-      end
-    end
-
     # fps_option (-r) は -i より前
     def ffmpeg_command(in_file, out_file)
-      "ffmpeg -v warning -hide_banner #{fps_option} -y -i #{in_file} -c:v libx264 -pix_fmt yuv420p -y #{out_file}"
     end
 
     # def fps_option
@@ -89,24 +110,16 @@ module Bioshogi
     # 1手 1.0 秒 → "-r 60/60"
     # 1手 1.5 秒 → "-r 60/90"
     def fps_option
-      if v = params[:video_speed].presence
-        v = (one_second * v.to_f).to_i
-        "-r #{one_second}/#{v}" # FIXME: -framerate をつかうべき？
-      end
+      v = (one_second * video_speed).to_i
+      "-r #{one_second}/#{v}" # -framerate だと動かない。-framerate は連番のとき用っぽい
     end
 
-    def in_out_tempfile
-      Tempfile.create(["i_", ".mp4"]) do |i|
-        Tempfile.create(["o_", ".mp4"]) do |o|
-          i = Pathname(i.path)
-          o = Pathname(o.path)
-          yield i, o
-          o.read
-        end
-      end
+    def video_speed
+      params[:video_speed].to_f
     end
 
     def strict_system(command)
+      puts command
       status, stdout, stderr = systemu(command)
       if !status.success?
         raise StandardError, stderr.strip
