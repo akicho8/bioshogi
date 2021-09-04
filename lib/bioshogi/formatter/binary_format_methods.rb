ac0fad5e087d8957734b711e67b257cbb53eecee
akicho8 <akicho8@gmail.com>
2021-09-04 19:46:10 +0900

    [test][fix] AnimationFormatter は消したのでテストも消す

--- lib/bioshogi/formatter/binary_format_methods.rb
+++ lib/bioshogi/formatter/binary_format_methods.rb
@@ -9,16 +9,6 @@ module Bioshogi
         image_formatter(options).to_blob_binary
       end
 
-      # DEPRECATION
-      def animation_formatter(options = {})
-        AnimationFormatter.render(self, options)
-      end
-
-      # DEPRECATION
-      def to_animation(options = {})
-        animation_formatter(options).to_blob_binary
-      end
-
       ################################################################################
 
       def to_png(options = {})
