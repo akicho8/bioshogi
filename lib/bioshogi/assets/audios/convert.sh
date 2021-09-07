b3c850d0d444c3d76c982be2d0e7408260530c6e
akicho8 <akicho8@gmail.com>
2021-09-07 22:01:37 +0900

    [feat][assets] 曲追加

---
+++ lib/bioshogi/assets/audios/convert.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+ffmpeg -hide_banner -i nc799.mp3    -c:a aac -strict experimental -vn -y nc799.m4a
+ffmpeg -hide_banner -i nc43122.mp3  -c:a aac -strict experimental -vn -y nc43122.m4a
+ffmpeg -hide_banner -i nc105702.mp3 -c:a aac -strict experimental -vn -y nc105702.m4a
+ffmpeg -hide_banner -i nc10812.mp3  -c:a aac -strict experimental -vn -y nc10812.m4a
+ffmpeg -hide_banner -i nc107860.mp3 -c:a aac -strict experimental -vn -y nc107860.m4a
+ffmpeg -hide_banner -i nc3366.mp3   -c:a aac -strict experimental -vn -y nc3366.m4a
+ffmpeg -hide_banner -i nc35943.mp3  -c:a aac -strict experimental -vn -y nc35943.m4a
+ffmpeg -hide_banner -i nc768.mp3    -c:a aac -strict experimental -vn -y nc768.m4a
+ffmpeg -hide_banner -i nc770.mp3    -c:a aac -strict experimental -vn -y nc770.m4a
+ffmpeg -hide_banner -i nc162705.mp3 -c:a aac -strict experimental -vn -y nc162705.m4a
+ffmpeg -hide_banner -i nc55257.wav  -c:a aac -strict experimental -vn -y nc55257.m4a
+ffmpeg -hide_banner -i ds4712.mp3   -c:a aac -strict experimental -vn -y ds4712.m4a
+ffmpeg -hide_banner -i ds7615.mp3   -c:a aac -strict experimental -vn -y ds7615.m4a
