67596cef53c4ea1dd35f6334ef049f6b1fd6e6b3
akicho8 <akicho8@gmail.com>
2021-09-08 20:20:52 +0900

    [feat] 和風BGM追加

--- lib/bioshogi/assets/audios/convert.sh
+++ lib/bioshogi/assets/audios/convert.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
-ffmpeg -hide_banner -i nc43122.mp3  -c:a aac -strict experimental -vn -y nc43122.m4a
-ffmpeg -hide_banner -i nc105702.mp3 -c:a aac -strict experimental -vn -y nc105702.m4a
-ffmpeg -hide_banner -i nc10812.mp3  -c:a aac -strict experimental -vn -y nc10812.m4a
-ffmpeg -hide_banner -i nc107860.mp3 -c:a aac -strict experimental -vn -y nc107860.m4a
-ffmpeg -hide_banner -i nc3366.mp3   -c:a aac -strict experimental -vn -y nc3366.m4a
-ffmpeg -hide_banner -i nc35943.mp3  -c:a aac -strict experimental -vn -y nc35943.m4a
-ffmpeg -hide_banner -i nc768.mp3    -c:a aac -strict experimental -vn -y nc768.m4a
-ffmpeg -hide_banner -i nc770.mp3    -c:a aac -strict experimental -vn -y nc770.m4a
-ffmpeg -hide_banner -i nc162705.mp3 -c:a aac -strict experimental -vn -y nc162705.m4a
-ffmpeg -hide_banner -i nc55257.wav  -c:a aac -strict experimental -vn -y nc55257.m4a
-ffmpeg -hide_banner -i ds4712.mp3   -c:a aac -strict experimental -vn -y ds4712.m4a
-ffmpeg -hide_banner -i ds7615.mp3   -c:a aac -strict experimental -vn -y ds7615.m4a
-ffmpeg -hide_banner -i ds3895.mp3   -c:a aac -strict experimental -vn -y ds3895.m4a
-ffmpeg -hide_banner -i ds13037.mp3  -c:a aac -strict experimental -vn -y ds13037.m4a
-ffmpeg -hide_banner -i ds4000.mp3   -c:a aac -strict experimental -vn -y ds4000.m4a
-ffmpeg -hide_banner -i ds12450.mp3  -c:a aac -strict experimental -vn -y ds12450.m4a
+
+# ffmpeg -hide_banner -i nc43122.mp3  -c:a aac -strict experimental -vn -y nc43122.m4a
+# ffmpeg -hide_banner -i nc105702.mp3 -c:a aac -strict experimental -vn -y nc105702.m4a
+# ffmpeg -hide_banner -i nc10812.mp3  -c:a aac -strict experimental -vn -y nc10812.m4a
+# ffmpeg -hide_banner -i nc107860.mp3 -c:a aac -strict experimental -vn -y nc107860.m4a
+# ffmpeg -hide_banner -i nc3366.mp3   -c:a aac -strict experimental -vn -y nc3366.m4a
+# ffmpeg -hide_banner -i nc35943.mp3  -c:a aac -strict experimental -vn -y nc35943.m4a
+# ffmpeg -hide_banner -i nc768.mp3    -c:a aac -strict experimental -vn -y nc768.m4a
+# ffmpeg -hide_banner -i nc770.mp3    -c:a aac -strict experimental -vn -y nc770.m4a
+# ffmpeg -hide_banner -i nc162705.mp3 -c:a aac -strict experimental -vn -y nc162705.m4a
+# ffmpeg -hide_banner -i nc55257.wav  -c:a aac -strict experimental -vn -y nc55257.m4a
+# ffmpeg -hide_banner -i ds4712.mp3   -c:a aac -strict experimental -vn -y ds4712.m4a
+# ffmpeg -hide_banner -i ds7615.mp3   -c:a aac -strict experimental -vn -y ds7615.m4a
+# ffmpeg -hide_banner -i ds3895.mp3   -c:a aac -strict experimental -vn -y ds3895.m4a
+# ffmpeg -hide_banner -i ds13037.mp3  -c:a aac -strict experimental -vn -y ds13037.m4a
+# ffmpeg -hide_banner -i ds4000.mp3   -c:a aac -strict experimental -vn -y ds4000.m4a
+# ffmpeg -hide_banner -i ds12450.mp3  -c:a aac -strict experimental -vn -y ds12450.m4a
+
+ffmpeg -hide_banner -i ds5837.mp3             -c:a aac -strict experimental -vn -y ds5837.m4a
+ffmpeg -hide_banner -i ds5616.mp3             -c:a aac -strict experimental -vn -y ds5616.m4a
+ffmpeg -hide_banner -i nc142538.mp3           -c:a aac -strict experimental -vn -y nc142538.m4a
+ffmpeg -hide_banner -i nc176917.mp3           -c:a aac -strict experimental -vn -y nc176917.m4a
+ffmpeg -hide_banner -i shw_akatsuki_japan.mp3 -c:a aac -strict experimental -vn -y shw_akatsuki_japan.m4a
