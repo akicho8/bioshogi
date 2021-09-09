c6dbdbec2c6150a36380bd91e054ba27d7682381
akicho8 <akicho8@gmail.com>
2021-09-09 13:58:08 +0900

    [fix] Media モジュールがグローバルスペースで定義されている

--- lib/bioshogi/media.rb
+++ lib/bioshogi/media.rb
@@ -1,40 +1,42 @@
-module Media
-  extend self
+module Bioshogi
+  module Media
+    extend self
 
-  def duration(file)
-    info = JSON.parse(`ffprobe -v warning -print_format json -show_entries format=duration #{file}`)
-    info["format"]["duration"].to_f
-  end
+    def duration(file)
+      info = JSON.parse(`ffprobe -v warning -print_format json -show_entries format=duration #{file}`)
+      info["format"]["duration"].to_f
+    end
 
-  def bit_rate(file)
-    info = JSON.parse(`ffprobe -v warning -print_format json -show_entries format=bit_rate #{file}`)
-    info["format"]["bit_rate"].to_f
-  end
+    def bit_rate(file)
+      info = JSON.parse(`ffprobe -v warning -print_format json -show_entries format=bit_rate #{file}`)
+      info["format"]["bit_rate"].to_f
+    end
 
-  def pretty_inspect(file)
-    JSON.parse(`ffprobe -v warning -pretty -print_format json -show_streams #{file}`)
-  end
+    def pretty_inspect(file)
+      JSON.parse(`ffprobe -v warning -pretty -print_format json -show_streams #{file}`)
+    end
 
-  def inspect(file)
-    `ffprobe -hide_banner #{file} 2>&1 | grep Stream`
-  end
+    def inspect(file)
+      `ffprobe -hide_banner #{file} 2>&1 | grep Stream`
+    end
 
-  def p(file)
-    puts inspect(file)
-  end
+    def p(file)
+      puts inspect(file)
+    end
 
-  def format(file)
-    JSON.parse(`ffprobe -v warning -pretty -print_format json -show_format #{file}`)["format"]
-  end
+    def format(file)
+      JSON.parse(`ffprobe -v warning -pretty -print_format json -show_format #{file}`)["format"]
+    end
 
-  # [Parsed_volumedetect_0 @ 0x7fd26b623100] n_samples: 423808
-  # [Parsed_volumedetect_0 @ 0x7fd26b623100] mean_volume: -16.1 dB
-  # [Parsed_volumedetect_0 @ 0x7fd26b623100] max_volume: -0.1 dB
-  # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_0db: 44
-  # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_1db: 71
-  # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_2db: 71
-  # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_3db: 242
-  def volume_info(file)
-    `ffmpeg -hide_banner -i #{file} -vn -af volumedetect -f null -`
+    # [Parsed_volumedetect_0 @ 0x7fd26b623100] n_samples: 423808
+    # [Parsed_volumedetect_0 @ 0x7fd26b623100] mean_volume: -16.1 dB
+    # [Parsed_volumedetect_0 @ 0x7fd26b623100] max_volume: -0.1 dB
+    # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_0db: 44
+    # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_1db: 71
+    # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_2db: 71
+    # [Parsed_volumedetect_0 @ 0x7fd26b623100] histogram_3db: 242
+    def volume_info(file)
+      `ffmpeg -hide_banner -i #{file} -vn -af volumedetect -f null -`
+    end
   end
 end
