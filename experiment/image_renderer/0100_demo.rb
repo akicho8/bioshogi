f12632993f0495b7379d3b5229960440220a91b6
akicho8 <akicho8@gmail.com>
2021-10-08 15:47:20 +0900

    [chore] demo/* 生成

--- experiment/image_renderer/0100_demo.rb
+++ experiment/image_renderer/0100_demo.rb
@@ -9,11 +9,11 @@ end
 
 parser = Parser.parse(SFEN1)
 ImageRenderer::ColorThemeInfo.each do |e|
-  bin = parser.to_png(color_theme_key: e.key)
-  output(bin, "color_theme/#{'%02d' % e.code}_#{e.key}.png")
+  bin = parser.to_png(color_theme_key: e.key, width: 1920, height: 1080, soldier_font_bold: true, stand_piece_font_bold: true)
+  output(bin, "color_theme/#{e.key}.png")
 end
 
 ImageRenderer::CanvasPatternInfo.each do |e|
   bin = e.execute.to_blob { |e| e.format = "png" }
-  output(bin, "canvas_pattern/#{'%02d' % e.code}_#{e.key}.png")
+  output(bin, "canvas_pattern/#{e.key}.png")
 end
