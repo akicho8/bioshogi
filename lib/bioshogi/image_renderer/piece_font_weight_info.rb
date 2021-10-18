efcf1a9c3c5c6d24d3298126d4d47079932ebbe9
akicho8 <akicho8@gmail.com>
2021-10-19 08:19:32 +0900

    [refactor] APIレベルのでの boolean 型は破綻する

--- lib/bioshogi/image_renderer/piece_font_weight_info.rb
+++ lib/bioshogi/image_renderer/piece_font_weight_info.rb
@@ -4,8 +4,8 @@ module Bioshogi
       include ApplicationMemoryRecord
       memory_record [
         { key: :is_piece_font_weight_auto, name: "おまかせ", to_params: {                                                         }, },
-        { key: :is_piece_font_weight_off,  name: "細字",     to_params: { soldier_font_bold: false, stand_piece_font_bold: false, }, },
-        { key: :is_piece_font_weight_on,   name: "太字",     to_params: { soldier_font_bold: true, stand_piece_font_bold: true,   }, },
+        { key: :is_piece_font_weight_normal,  name: "細字",     to_params: { soldier_font_bold: false, stand_piece_font_bold: false, }, },
+        { key: :is_piece_font_weight_bold,   name: "太字",     to_params: { soldier_font_bold: true, stand_piece_font_bold: true,   }, },
       ]
     end
   end
