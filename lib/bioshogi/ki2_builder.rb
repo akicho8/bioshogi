b8e96fdf0ef338d0b8627c4d5bf384ef6fa2ae31
akicho8 <akicho8@gmail.com>
2021-10-26 12:16:13 +0900

    [refactor] cols -> column_count

--- lib/bioshogi/ki2_builder.rb
+++ lib/bioshogi/ki2_builder.rb
@@ -6,7 +6,7 @@ module Bioshogi
 
     def self.default_params
       super.merge({
-          :cols        => 10,
+          :column_count        => 10,
           :fixed_width => nil,
           :same_suffix => "　",
           :header_skip => false,
@@ -30,7 +30,7 @@ module Bioshogi
 
       if @params[:fixed_width]
         # 固定幅で整列(値が小さいと揃わない場合がある)
-        out << @parser.mediator.hand_logs.group_by.with_index {|_, i| i / @params[:cols] }.values.collect { |v|
+        out << @parser.mediator.hand_logs.group_by.with_index {|_, i| i / @params[:column_count] }.values.collect { |v|
           v.collect { |e|
             s = e.to_ki2(with_location: true, same_suffix: @params[:same_suffix])
             @parser.mb_ljust(s, @params[:fixed_width])
@@ -42,7 +42,7 @@ module Bioshogi
           e.to_ki2(with_location: true, same_suffix: @params[:same_suffix])
         end
 
-        list2 = list.in_groups_of(@params[:cols])
+        list2 = list.in_groups_of(@params[:column_count])
         column_widths = list2.transpose.collect do |e|
           e.collect { |e| e.to_s.toeuc.bytesize }.max
         end
