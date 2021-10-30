f21b4bfe15f4c69b60d7334bb5a1c86b27f4526c
akicho8 <akicho8@gmail.com>
2021-10-30 13:26:12 +0900

    [feat] コードカバレッジを広げる

--- spec/animation_support.rb
+++ spec/animation_support.rb
@@ -13,7 +13,7 @@ module AnimationSupport
   # 6ページ  * 1ページあたり0.5 = トータル3秒
   def target1(ext_name, method, params = {})
     info = Bioshogi:: Parser.parse("position startpos moves 7g7f 8c8d")
-    bin = info.send(method, cover_text: "(cover_text)", page_duration: 0.5, end_duration: 1, width: 2, height: 2, **params)
+    bin = info.send(method, cover_text: "(cover_text)", page_duration: 0.5, end_duration: 1, width: 2, height: 2, audio_theme_key: "is_audio_theme_ds3479", turn_embed_key: "is_turn_embed_on", **params)
     filename = Pathname("_outout.#{ext_name}")
     filename.write(bin)
     Bioshogi::Media.streams(filename).tap do |e|
