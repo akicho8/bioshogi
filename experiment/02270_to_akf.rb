7f2c3ab484c6629149e0b3e4f0d5f387679bbf89
akicho8 <akicho8@gmail.com>
2021-11-05 21:09:26 +0900

    [feat] to_akf 改良

--- experiment/02270_to_akf.rb
+++ experiment/02270_to_akf.rb
@@ -2,19 +2,26 @@ require "./setup"
 
 info = Parser.parse("先手：alice\n先手の持駒：銀\n\n68銀 34歩 76歩 88角不成 55銀打 55角成")
 tp info.to_akf[:header]
-tp info.to_akf[:moves][0]
-tp info.to_akf[:moves][1]
-tp info.to_akf[:moves]
-# >> |------------+----------------------------------|
-# >> |       先手 | alice                            |
-# >> | 先手の持駒 | 銀                               |
-# >> | 先手の囲い | 居玉                             |
-# >> | 後手の囲い | 居玉                             |
-# >> | 先手の戦型 | 嬉野流                           |
-# >> | 先手の備考 | 居飛車, 相居飛車, 相居玉         |
-# >> | 後手の備考 | 角不成, 居飛車, 相居飛車, 相居玉 |
-# >> |------------+----------------------------------|
+# tp info.to_akf[:moves][0]
+# tp info.to_akf[:moves][1]
+tp info.to_akf[:moves].first
+tp info.to_akf[:moves].drop(1)
+tp info.to_akf.except(:moves)
+# >> |----------------------+----------------------------------|
+# >> |                 先手 | alice                            |
+# >> |           先手の持駒 | 銀                               |
+# >> |           先手の囲い | 居玉                             |
+# >> |           後手の囲い | 居玉                             |
+# >> |           先手の戦型 | 嬉野流                           |
+# >> |           先手の備考 | 居飛車, 相居飛車, 相居玉         |
+# >> |           後手の備考 | 角不成, 居飛車, 相居飛車, 相居玉 |
+# >> |                 手数 | 6                                |
+# >> | last_action_kif_word | 投了                             |
+# >> |     judgment_message | まで6手で後手の勝ち              |
+# >> |           error_text |                                  |
+# >> |----------------------+----------------------------------|
 # >> |---------------+-------------------------------------------------------------------------------|
+# >> |         index | 0                                                                             |
 # >> |   human_index | 0                                                                             |
 # >> |    place_same |                                                                               |
 # >> | total_seconds | 0                                                                             |
@@ -23,33 +30,16 @@ tp info.to_akf[:moves]
 # >> |  history_sfen | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 |
 # >> | snapshot_sfen | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 |
 # >> |---------------+-------------------------------------------------------------------------------|
-# >> |---------------+------------------------------------------------------------------------------------------|
-# >> |   human_index | 1                                                                                        |
-# >> |    place_same |                                                                                          |
-# >> |     _location | black                                                                                    |
-# >> |          type | t_move                                                                                   |
-# >> |         piece | silver                                                                                   |
-# >> |      promoted | false                                                                                    |
-# >> |          from | {:x=>7, :y=>9}                                                                           |
-# >> |            to | {:x=>6, :y=>8}                                                                           |
-# >> |      captured |                                                                                          |
-# >> |         _sfen | 7i6h                                                                                     |
-# >> |          _kif | ▲６八銀(79)                                                                             |
-# >> |          _csa | +7968GI                                                                                  |
-# >> | total_seconds | 0                                                                                        |
-# >> |  used_seconds | 0                                                                                        |
-# >> |         skill | {:defense=>[], :attack=>[], :technique=>[], :note=>[]}                                   |
-# >> |  history_sfen | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h |
-# >> | snapshot_sfen | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B1S3R1/LN1GKGSNL w S 2          |
-# >> |---------------+------------------------------------------------------------------------------------------|
-# >> |-------------+------------+---------------+--------------+--------------------------------------------------------+--------------------------------------------------------------------------------------------------------------------+-------------------------------------------------------------------------------------+-----------+-----------------+--------+----------+----------------+----------------+--------------------------------------------------------------------------------+-------+----------------+---------|
-# >> | human_index | place_same | total_seconds | used_seconds | skill                                                  | history_sfen                                                                                                       | snapshot_sfen                                                                       | _location | type            | piece  | promoted | from           | to             | captured                                                                       | _sfen | _kif           | _csa    |
-# >> |-------------+------------+---------------+--------------+--------------------------------------------------------+--------------------------------------------------------------------------------------------------------------------+-------------------------------------------------------------------------------------+-----------+-----------------+--------+----------+----------------+----------------+--------------------------------------------------------------------------------+-------+----------------+---------|
-# >> |           0 |            |             0 |              |                                                        | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1                                      | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1       |           |                 |        |          |                |                |                                                                                |       |                |         |
-# >> |           1 |            |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h                           | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B1S3R1/LN1GKGSNL w S 2     | black     | t_move          | silver | false    | {:x=>7, :y=>9} | {:x=>6, :y=>8} |                                                                                | 7i6h  | ▲６八銀(79)   | +7968GI |
-# >> |           2 | false      |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d                      | position sfen lnsgkgsnl/1r5b1/pppppp1pp/6p2/9/9/PPPPPPPPP/1B1S3R1/LN1GKGSNL b S 3   | white     | t_move          | pawn   | false    | {:x=>3, :y=>3} | {:x=>3, :y=>4} |                                                                                | 3c3d  | △３四歩(33)   | -3334FU |
-# >> |           3 | false      |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f                 | position sfen lnsgkgsnl/1r5b1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1B1S3R1/LN1GKGSNL w S 4 | black     | t_move          | pawn   | false    | {:x=>7, :y=>7} | {:x=>7, :y=>6} |                                                                                | 7g7f  | ▲７六歩(77)   | +7776FU |
-# >> |           4 | false      |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f 2b8h            | position sfen lnsgkgsnl/1r7/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1b1S3R1/LN1GKGSNL b Sb 5  | white     | t_promote_throw | bishop | false    | {:x=>2, :y=>2} | {:x=>8, :y=>8} | {:piece=>:bishop, :promoted=>false, :place=>{:x=>8, :y=>8}, :location=>:black} | 2b8h  | △８八角(22)   | -2288KA |
-# >> |           5 | false      |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f 2b8h S*5e       | position sfen lnsgkgsnl/1r7/pppppp1pp/6p2/4S4/2P6/PP1PPPPPP/1b1S3R1/LN1GKGSNL w b 6 | black     | t_drop          | silver |          |                | {:x=>5, :y=>5} |                                                                                | S*5e  | ▲５五銀打     | +0055GI |
-# >> |           6 | true       |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f 2b8h S*5e 8h5e+ | position sfen lnsgkgsnl/1r7/pppppp1pp/6p2/4+b4/2P6/PP1PPPPPP/3S3R1/LN1GKGSNL b bs 7 | white     | t_promote_on    | bishop | false    | {:x=>8, :y=>8} | {:x=>5, :y=>5} | {:piece=>:silver, :promoted=>false, :place=>{:x=>5, :y=>5}, :location=>:black} | 8h5e+ | △５五角成(88) | -8855UM |
-# >> |-------------+------------+---------------+--------------+--------------------------------------------------------+--------------------------------------------------------------------------------------------------------------------+-------------------------------------------------------------------------------------+-----------+-----------------+--------+----------+----------------+----------------+--------------------------------------------------------------------------------+-------+----------------+---------|
+# >> |-------+-------------+------------+-----------+-----------------+--------+----------+----------------+----------------+--------------------------------------------------------------------------------+-------+----------------+---------+---------------+--------------+--------------------------------------------------------+--------------------------------------------------------------------------------------------------------------------+-------------------------------------------------------------------------------------|
+# >> | index | human_index | place_same | _location | type            | piece  | promoted | from           | to             | captured                                                                       | _sfen | _kif           | _csa    | total_seconds | used_seconds | skill                                                  | history_sfen                                                                                                       | snapshot_sfen                                                                       |
+# >> |-------+-------------+------------+-----------+-----------------+--------+----------+----------------+----------------+--------------------------------------------------------------------------------+-------+----------------+---------+---------------+--------------+--------------------------------------------------------+--------------------------------------------------------------------------------------------------------------------+-------------------------------------------------------------------------------------|
+# >> |     1 |           1 |            | black     | t_move          | silver | false    | {:x=>7, :y=>9} | {:x=>6, :y=>8} |                                                                                | 7i6h  | ▲６八銀(79)   | +7968GI |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h                           | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B1S3R1/LN1GKGSNL w S 2     |
+# >> |     2 |           2 | false      | white     | t_move          | pawn   | false    | {:x=>3, :y=>3} | {:x=>3, :y=>4} |                                                                                | 3c3d  | △３四歩(33)   | -3334FU |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d                      | position sfen lnsgkgsnl/1r5b1/pppppp1pp/6p2/9/9/PPPPPPPPP/1B1S3R1/LN1GKGSNL b S 3   |
+# >> |     3 |           3 | false      | black     | t_move          | pawn   | false    | {:x=>7, :y=>7} | {:x=>7, :y=>6} |                                                                                | 7g7f  | ▲７六歩(77)   | +7776FU |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f                 | position sfen lnsgkgsnl/1r5b1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1B1S3R1/LN1GKGSNL w S 4 |
+# >> |     4 |           4 | false      | white     | t_promote_throw | bishop | false    | {:x=>2, :y=>2} | {:x=>8, :y=>8} | {:piece=>:bishop, :promoted=>false, :place=>{:x=>8, :y=>8}, :location=>:black} | 2b8h  | △８八角(22)   | -2288KA |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f 2b8h            | position sfen lnsgkgsnl/1r7/pppppp1pp/6p2/9/2P6/PP1PPPPPP/1b1S3R1/LN1GKGSNL b Sb 5  |
+# >> |     5 |           5 | false      | black     | t_drop          | silver |          |                | {:x=>5, :y=>5} |                                                                                | S*5e  | ▲５五銀打     | +0055GI |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f 2b8h S*5e       | position sfen lnsgkgsnl/1r7/pppppp1pp/6p2/4S4/2P6/PP1PPPPPP/1b1S3R1/LN1GKGSNL w b 6 |
+# >> |     6 |           6 | true       | white     | t_promote_on    | bishop | false    | {:x=>8, :y=>8} | {:x=>5, :y=>5} | {:piece=>:silver, :promoted=>false, :place=>{:x=>5, :y=>5}, :location=>:black} | 8h5e+ | △５五角成(88) | -8855UM |             0 |            0 | {:defense=>[], :attack=>[], :technique=>[], :note=>[]} | position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b S 1 moves 7i6h 3c3d 7g7f 2b8h S*5e 8h5e+ | position sfen lnsgkgsnl/1r7/pppppp1pp/6p2/4+b4/2P6/PP1PPPPPP/3S3R1/LN1GKGSNL b bs 7 |
+# >> |-------+-------------+------------+-----------+-----------------+--------+----------+----------------+----------------+--------------------------------------------------------------------------------+-------+----------------+---------+---------------+--------------+--------------------------------------------------------+--------------------------------------------------------------------------------------------------------------------+-------------------------------------------------------------------------------------|
+# >> |--------+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+# >> | header | {"先手"=>"alice", "先手の持駒"=>"銀", "先手の囲い"=>"居玉", "後手の囲い"=>"居玉", "先手の戦型"=>"嬉野流", "先手の備考"=>"居飛車, 相居飛車, 相居玉", "後手の備考"=>"角不成, 居飛車, 相居飛車, 相居玉", "手数"=>6, :last_action_kif_word=>"投了", :judgment_message=>"まで6手で後手の勝ち", :error_text=>nil} |
+# >> |--------+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
--- lib/bioshogi/akf_builder.rb
+++ lib/bioshogi/akf_builder.rb
@@ -21,35 +21,53 @@ module Bioshogi
       @parser.mediator_board_setup(@mediator2)
 
       @hv = {}
-      @hv[:header] = @parser.header.to_h
+      @hv[:header] = @parser.header.to_h.clone
+      @hv[:header]["手数"] = @parser.move_infos.size
 
       @chess_clock = ChessClock.new
 
       @hv[:moves] = []
       @hv[:moves] << {
+        :index         => 0,
         :human_index   => @mediator2.initial_state_turn_info.display_turn,
         :place_same    => nil,
         **@chess_clock.last_clock.to_h,
         :total_seconds => 0,
         :used_seconds  => nil,
         :skill         => nil,
-        :history_sfen    => @mediator2.to_history_sfen,
-        :snapshot_sfen    => @mediator2.to_snapshot_sfen,
+        :history_sfen  => @mediator2.to_history_sfen,
+        :snapshot_sfen => @mediator2.to_snapshot_sfen,
       }
       @hv[:moves] += @parser.move_infos.collect.with_index do |info, i|
         @mediator2.execute(info[:input], used_seconds: @parser.used_seconds_at(i))
         @chess_clock.add(@parser.used_seconds_at(i))
         hand_log = @mediator2.hand_logs.last
         {
-          :human_index => @mediator2.initial_state_turn_info.display_turn + i.next,
-          :place_same  => hand_log.place_same,
+          :index         => i.next,
+          :human_index   => @mediator2.initial_state_turn_info.display_turn + i.next,
+          :place_same    => hand_log.place_same,
           **hand_log.to_akf,
           **@chess_clock.last_clock.to_h,
-          :skill => hand_log.skill_set.to_h,
-          :history_sfen => @mediator2.to_history_sfen,
+          :skill         => hand_log.skill_set.to_h,
+          :history_sfen  => @mediator2.to_history_sfen,
           :snapshot_sfen => @mediator2.to_snapshot_sfen,
         }
       end
+
+      # if @parser.last_status_params
+      #   if used_seconds = @parser.last_status_params[:used_seconds]
+      #     if @chess_clock
+      #       @chess_clock.add(used_seconds)
+      #       right_part = @chess_clock.to_s
+      #     end
+      #   end
+      # end
+
+      @hv[:header] = @hv[:header].merge({
+          :last_action_kif_word => @parser.last_action_info.kif_word,
+          :judgment_message     => @parser.judgment_message,
+          :error_text           => @parser.error_message_part,
+        })
 
       @hv
     end
