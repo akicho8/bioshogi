5a838d32810960384f3560da74a0e1012f8b3c48
akicho8 <akicho8@gmail.com>
2021-12-19 13:01:09 +0900

    Revert "[feat] 表紙に絵文字が含まれるとエラーとする"
    This reverts commit 497291d2e2349d478795eccf3f60e82dc8569a2e.

--- experiment/cover_renderer/0100_cover_text.rb
+++ experiment/cover_renderer/0100_cover_text.rb
@@ -1,11 +1,3 @@
 require "../setup"
 
 CoverRenderer.new(text: "なんとか　将棋大会\n☗先手 vs ☖後手\n2021-09-12\nabcdefghijklmnopqrstuvwxyz").display
-# ~> /Users/ikeda/src/bioshogi/lib/bioshogi/cover_renderer/base.rb:40:in `text_normalize': 絵文字と思われる画像化できない文字が含まれています : "なんとか　将棋大会\n☗先手 vs ☖後手\n2021-09-12\nabcdefghijklmnopqrstuvwxyz" (ArgumentError)
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/cover_renderer/main_text_methods.rb:32:in `text'
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/cover_renderer/main_text_methods.rb:28:in `main_text_render'
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/cover_renderer/base.rb:28:in `render'
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/image_renderer/helper.rb:18:in `display'
-# ~> 	from -:7:in `<main>'
-# >> "なんとか　将棋大会\n☗先手 vs ☖後手\n2021-09-12\nabcdefghijklmnopqrstuvwxyz"
-# >> "なんとか　将棋大会\n先手 vs 後手\n2021-09-12\nabcdefghijklmnopqrstuvwxyz"
--- lib/bioshogi/cover_renderer/base.rb
+++ lib/bioshogi/cover_renderer/base.rb
@@ -32,16 +32,9 @@ module Bioshogi
 
       private
 
+      # Ricty フォントでは全角が "「」" になってしまうため透明にする
       def text_normalize(str)
-        s = str.to_s
-
-        # 絵文字が含まれる場合はエラーとする
-        if s != emoji_reject(s)
-          raise ArgumentError, "絵文字と思われる画像化できない文字が含まれています : #{str.inspect}"
-        end
-
-        # Ricty フォントでは全角が "「」" になってしまうため透明にする
-        s.gsub(/\u3000/, "  ")
+        str.to_s.gsub(/\u3000/, "  ")
       end
 
       def canvas_layer_create
@@ -61,10 +54,6 @@ module Bioshogi
 
       def ext_name
         params[:image_format]
-      end
-
-      def emoji_reject(str)
-        str.to_s.encode("EUC-JP", "UTF-8", invalid: :replace, undef: :replace, replace: "").encode("UTF-8")
       end
     end
   end
