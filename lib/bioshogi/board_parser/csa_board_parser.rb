3e81ab1244803a5f5a47d0de135a9ce52e8542d9
akicho8 <akicho8@gmail.com>
2022-01-02 22:12:12 +0900

    [feat] スペースが潰れているCSAに対応

--- lib/bioshogi/board_parser/csa_board_parser.rb
+++ lib/bioshogi/board_parser/csa_board_parser.rb
@@ -18,10 +18,18 @@ module Bioshogi
 
       def parse
         shape_lines.each do |line|
-          if md = line.match(/P(?<y>\d+)(?<cells>.*)/)
+          if md = line.match(/P(?<y>\d+)\s*(?<cells>.*)\s*/) # strip 代わりに cells の両方を \s* している
             y = md[:y]
+
             # 空白または * の文字を 1..3 とすることで行末スペースの有無に依存しなくなる
-            cells = md[:cells].scan(/\S{3}|[\s\*]{1,3}/)
+            # この方法では次のようにスペースが潰れている場合に失敗する
+            # P1 * -HI * * * * * -KA *
+            # cells = md[:cells].scan(/\S{3}|[\s\*]{1,3}/)
+
+            # 改良した方法
+            # " " または +- の直前で分離する
+            cells = md[:cells].split(/\s+|(?=[+-])/)
+
             cells.reverse_each.with_index(1) do |e, x|
               if md = e.match(/(?<csa_sign>\S)(?<piece>\S{2})/)
                 location = Location[md[:csa_sign]]
