df102888faa1a86b24c40600772ed0f5fa86e0de
akicho8 <akicho8@gmail.com>
2022-07-10 13:21:30 +0900

    [refactor] cosmetic change

--- lib/bioshogi/image_renderer/palette_info.rb
+++ lib/bioshogi/image_renderer/palette_info.rb
@@ -4,7 +4,7 @@ module Bioshogi
       include ApplicationMemoryRecord
       memory_record [
         { key: "主用駒色", hsl: [ 43, 100, 81], },
-        { key: "主用朱色", hsl: [358, 84, 60],  },
+        { key: "主用朱色", hsl: [358,  84, 60], },
       ]
 
       def to_color
