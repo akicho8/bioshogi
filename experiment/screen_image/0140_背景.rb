8f501ccac2acccb92dde36402d5106da48a15f3c
akicho8 <akicho8@gmail.com>
2022-12-22 22:00:40 +0900

    [refactor] experiment/screen_image/*

--- experiment/screen_image/0140_背景.rb
+++ experiment/screen_image/0140_背景.rb
@@ -4,10 +4,3 @@ require "color"
 parser = Parser.parse(SFEN1)
 parser.screen_image_renderer(bg_file: ASSETS_DIR.join("images/matrix.png"), color_theme_key: "orange_lcd_mode").display
 parser.screen_image_renderer(color_theme_key: "matrix_mode").display
-# ~> /Users/ikeda/src/bioshogi/lib/bioshogi/screen_image_renderer.rb:191:in `read': unable to open image `/ASSETS_DIR.join("images/matrix.png': No such file or directory @ error/blob.c/OpenBlob/2924 (Magick::ImageMagickError)
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/screen_image_renderer.rb:191:in `canvas_create'
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/screen_image_renderer.rb:152:in `render'
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/screen_image_renderer.rb:112:in `tap'
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/screen_image_renderer.rb:112:in `render'
-# ~> 	from /Users/ikeda/src/bioshogi/lib/bioshogi/formatter/binary_format_methods.rb:5:in `screen_image_renderer'
-# ~> 	from -:24:in `<main>'
--- experiment/screen_image/0170_速度確認.rb
+++ experiment/screen_image/0170_速度確認.rb
@@ -18,19 +18,10 @@ def test2
       # :xcontainer_class                 => Container::Fast,
     })
   object = info.screen_image_renderer({width: 100, height: 100})
-  object.to_blob[0..3]           # => 
+  object.to_blob_binary[0..3]           # => "\x89PNG", "\x89PNG", "\x89PNG", "\x89PNG", "\x89PNG"
 end
 
 require "active_support/core_ext/benchmark"
 def _; "%7.2f ms" % Benchmark.ms { 5.times { yield } } end
 _ { test1 } # => "   0.01 ms"
-_ { test2 } # => 
-# ~> -:21:in `test2': undefined method `to_blob' for #<Bioshogi::ScreenImage::Renderer:0x00007fd020e36f78> (NoMethodError)
-# ~> 	from -:27:in `block in <main>'
-# ~> 	from -:25:in `block (2 levels) in _'
-# ~> 	from -:25:in `times'
-# ~> 	from -:25:in `block in _'
-# ~> 	from /usr/local/var/rbenv/versions/2.6.5/lib/ruby/2.6.0/benchmark.rb:308:in `realtime'
-# ~> 	from /usr/local/var/rbenv/versions/2.6.5/lib/ruby/gems/2.6.0/gems/activesupport-6.1.7/lib/active_support/core_ext/benchmark.rb:14:in `ms'
-# ~> 	from -:25:in `_'
-# ~> 	from -:27:in `<main>'
+_ { test2 } # => "3144.33 ms"
