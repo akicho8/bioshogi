68d9842dbf62c82d7316ca0017ce0193a3eb18e8
akicho8 <akicho8@gmail.com>
2022-12-29 21:09:45 +0900

    [refactor] cosmetic change

--- lib/bioshogi/parser/sfen_parser.rb
+++ lib/bioshogi/parser/sfen_parser.rb
@@ -11,14 +11,14 @@ module Bioshogi
       end
 
       def parse
-        @mi.sfen_info = Sfen.parse(normalized_source)
-        @mi.move_infos = @mi.sfen_info.move_infos
+        @mi.sfen_info      = Sfen.parse(normalized_source)
+        @mi.move_infos     = @mi.sfen_info.move_infos
         @mi.force_handicap = @mi.sfen_info.handicap?
 
         @mi.sfen_info.piece_counts.each do |location_key, counts|
           location = Location.fetch(location_key)
           name = location.call_name(@mi.sfen_info.handicap?)
-          mi.header["#{name}の持駒"] = Piece.h_to_s(counts)
+          @mi.header["#{name}の持駒"] = Piece.h_to_s(counts)
         end
       end
     end
