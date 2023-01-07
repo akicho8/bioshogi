ff3a2dc78e747bd74ef199d3e92a492364bcd8f7
akicho8 <akicho8@gmail.com>
2023-01-07 11:03:20 +0900

    [refactor] 戦法ファイル正規化

--- lib/bioshogi/explain/file_normalizer.rb
+++ lib/bioshogi/explain/file_normalizer.rb
@@ -2,16 +2,17 @@ module Bioshogi
   module Explain
     class FileNormalizer
       def call
-        TacticInfo.all_elements.each do |e|
-          dir = Pathname("#{__dir__}/#{e.tactic_info.name}")
-          file = dir.glob("#{e.key}.{kif,ki2}").first
-          file = file.expand_path
-          if file
-            info = Parser.parse(file.read)
-            new_file = dir.join(file.sub_ext(".kif"))
-            new_file.write(info.to_kif)
-            puts new_file
-          end
+        TacticInfo.all_elements.each.with_index(&method(:normalize_one))
+      end
+
+      private
+
+      def normalize_one(elem, index)
+        if file = elem.sample_kif_or_ki2_file
+          info = Parser.parse(file.read)
+          new_file = file.sub_ext(".kif") # ki2 だった場合を考慮する
+          new_file.write(info.to_kif)
+          puts "[#{index.next} / #{TacticInfo.all_elements.size}] #{new_file}"
         end
       end
     end
