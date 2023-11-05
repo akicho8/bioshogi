50958671b510fcc983a2442fcaf66321d31541da
akicho8 <akicho8@gmail.com>
2023-11-05 11:16:09 +0900

    [doc] 棋譜変換

--- doc/0100_header.rb
+++ doc/0100_header.rb
@@ -1,4 +1,1 @@
-#+hidden: true
-require "./setup"
-
 #+title1: 将棋の棋譜相互変換、棋譜から戦術抽出、超弱いAIなどのライブラリ
--- doc/0110_code.rb
+++ doc/0110_code.rb
@@ -1,11 +1,10 @@
-# 将棋の棋譜相互変換、棋譜から戦術抽出、超弱いAIなどのライブラリ #
+#+hidden: true
+require "./setup"
 
-## 棋譜の読み取りと変換 ##
+#+title2: 棋譜の読み取りと変換
 
-```ruby
 info = Bioshogi::Parser.parse("76歩 34歩")
 info.to_kif   # => "先手の備考：居飛車, 相居飛車\n後手の備考：居飛車, 相居飛車\n手合割：平手\n手数----指手---------消費時間--\n   1 ７六歩(77)\n   2 ３四歩(33)\n   3 投了\nまで2手で後手の勝ち\n"
 info.to_sfen  # => "position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 7g7f 3c3d"
 info.to_csa   # => "V2.2\n' 手合割:平手\nPI\n+\n+7776FU\n-3334FU\n%TORYO\n"
 info.to_ki2   # => "先手の備考：居飛車, 相居飛車\n後手の備考：居飛車, 相居飛車\n手合割：平手\n\n▲７六歩 △３四歩\nまで2手で後手の勝ち\n"
-```
