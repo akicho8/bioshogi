82c2bf5cdb37b9d22d01087aca38e3482bba8ad8
akicho8 <akicho8@gmail.com>
2024-10-07 10:08:19 +0900

    [feat] [判定改善] 片美濃囲い 美濃囲い

--- lib/bioshogi/explain/tactic_validator.rb
+++ lib/bioshogi/explain/tactic_validator.rb
@@ -4,37 +4,52 @@ module Bioshogi
   module Explain
     class TacticValidator
       def call
-        rows = TacticInfo.all_elements.collect(&method(:process_one))
+        @rows = TacticInfo.all_elements.collect { |e| SingleValidator.new(e).call }
         puts
-        tp rows
-        puts rows.none? { |e| e["合致"] == "ERROR" } ? "OK" : "ERROR"
+        tp @rows
+        puts success? ? "OK" : "ERROR"
+        tp errors
+      end
+
+      def errors
+        @errors ||= @rows.find_all { |e| e["合致"] == "ERROR" }
+      end
+
+      def success?
+        errors.none?
       end
 
       private
 
-      def process_one(e)
-        file = e.sample_kif_or_ki2_file
-        row = { "合致" => "", key: e.key }
-        if file
-          row[:file] = file.basename.to_s
-          str = file.read
-          info = Parser.parse(str)
-          info.formatter.container_run_once
-          info.formatter.container.players.each do |player|
-            keys = player.skill_set.list_of(e).normalize.collect(&:key)
-            row[player.location.key] = keys
-          end
-          info.formatter.container.players.each do |player|
-            if row[player.location.key].include?(e.key)
-              row["合致"] += player.location.mark
+      class SingleValidator
+        def initialize(item)
+          @item = item
+        end
+
+        def call
+          file = @item.sample_kif_or_ki2_file
+          row = { "合致" => "", key: @item.key }
+          if file
+            # row[:file] = file.basename.to_s
+            str = file.read
+            info = Parser.parse(str)
+            info.formatter.container_run_once
+            info.formatter.container.players.each do |player|
+              keys = player.skill_set.list_of(@item).normalize.collect(&:key)
+              row[player.location.key] = keys
+            end
+            info.formatter.container.players.each do |player|
+              if row[player.location.key].include?(@item.key)
+                row["合致"] += player.location.mark
+              end
             end
           end
+          if row["合致"].blank?
+            row["合致"] = "ERROR"
+          end
+          print row["合致"] == "ERROR" ? "E" : "."
+          row
         end
-        if row["合致"].blank?
-          row["合致"] = "ERROR"
-        end
-        print row["合致"] == "ERROR" ? "E" : "."
-        row
       end
     end
   end
