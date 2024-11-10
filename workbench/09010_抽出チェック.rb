7bf442dc11b35d645ace25708001da859bfde0f8
akicho8 <akicho8@gmail.com>
2024-11-10 17:22:24 +0900

    [chore] 2ch棋譜の変換を行なって差分がないか確認するコードを追加する

---
+++ workbench/09010_抽出チェック.rb
@@ -0,0 +1,14 @@
+require "./setup"
+outout_dir = Pathname("~/src/2ch棋譜変換後").expand_path
+files = Pathname.glob("../../2chkifu/**/*.{ki2,KI2}")
+files = files.take(5000)
+files = files.collect(&:expand_path)
+files.each do |file|
+  info = Parser.file_parse(file, typical_error_case: :skip)
+  outfile = outout_dir.join(file.basename)
+  outfile.write(info.to_kif)
+  print "."
+  STDOUT.flush
+end
+puts
+# >> .
