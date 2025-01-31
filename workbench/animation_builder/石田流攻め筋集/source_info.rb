98d8ea8332948128a138719d07a6157f75233f03
akicho8 <akicho8@gmail.com>
2025-01-31 21:45:47 +0900

    [feat] [石田流攻め筋集] 対左美濃2を追加する

--- workbench/animation_builder/石田流攻め筋集/source_info.rb
+++ workbench/animation_builder/石田流攻め筋集/source_info.rb
@@ -3,6 +3,7 @@
 class SourceInfo
   include ApplicationMemoryRecord
   memory_record [
-    { key: "将棋大図書館", url: "https://www.shogilounge.com/entry/double-ranging-rook-third-file-rook", },
+    { key: "将棋大図書館",       url: "https://www.shogilounge.com/entry/double-ranging-rook-third-file-rook", },
+    { key: "三間飛車のポイント", url: "https://www.amazon.co.jp/dp/B0D41MHRT7", },
   ]
 end
