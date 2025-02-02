7258b1d51acea36c30a23ee565f617b0ddcecf58
akicho8 <akicho8@gmail.com>
2025-02-02 14:48:05 +0900

    [chore] [石田流攻め筋集] 対棒金3 を追加する

--- workbench/animation_builder/石田流攻め筋集/source_info.rb
+++ workbench/animation_builder/石田流攻め筋集/source_info.rb
@@ -5,5 +5,6 @@ class SourceInfo
   memory_record [
     { key: "将棋大図書館",       url: "https://www.shogilounge.com/entry/double-ranging-rook-third-file-rook", },
     { key: "三間飛車のポイント", url: "https://www.amazon.co.jp/dp/B0D41MHRT7", },
+    { key: "勝てる石田流",       url: "https://www.amazon.co.jp/dp/B072BFCYQR", },
   ]
 end
