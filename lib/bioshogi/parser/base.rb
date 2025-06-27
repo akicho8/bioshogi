74ec29c47c9f3942d9b71d87fc28ea46f06816e3
akicho8 <akicho8@gmail.com>
2025-06-27 15:25:43 +0900

    [feat] parser の analysis_feature の初期値に Bioshogi.analysis_feature を指定する

--- lib/bioshogi/parser/base.rb
+++ lib/bioshogi/parser/base.rb
@@ -26,7 +26,7 @@ module Bioshogi
             # false: 例外を出す(デフォルト)
             :typical_error_case => false,
 
-            :analysis_feature   => true,
+            :analysis_feature   => Bioshogi.analysis_feature,
             :ki2_function       => true, # ki2にしないのであれば指定するとかなり速くなる
 
             :validate_feature   => true, # 将棋ウォーズの棋譜なら指定すると少し速くなる
