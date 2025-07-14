45213d18d6458c9c18d822be95484e0ca81c2d71
akicho8 <akicho8@gmail.com>
2025-07-14 20:26:46 +0900

    [refactor] クラスの中であってもインスタンス変数を直接さわらないようにする

--- lib/bioshogi/analysis/finalize_tag_func_info.rb
+++ lib/bioshogi/analysis/finalize_tag_func_info.rb
@@ -12,7 +12,7 @@ module Bioshogi
           description: "「力戦」より前に判定する",
           func: -> {
             if win_side_location
-              player = @container.player_at(win_side_location)
+              player = container.player_at(win_side_location)
               if player.tag_bundle.attack_and_defense_is_blank?
                 player.tag_bundle << "名人に定跡なし"
               end
@@ -22,7 +22,7 @@ module Bioshogi
         {
           key: "力戦",
           func: -> {
-            @container.players.each do |e|
+            container.players.each do |e|
               if e.tag_bundle.attack_and_defense_is_blank?
                 e.tag_bundle << "力戦"
               end
@@ -35,7 +35,7 @@ module Bioshogi
         {
           key: "居飛車",
           func: -> {
-            @container.players.each do |e|
+            container.players.each do |e|
               if !e.tag_bundle.include?("振り飛車") && !e.tag_bundle.include?("居飛車")
                 e.tag_bundle << "居飛車"
               end
@@ -45,8 +45,8 @@ module Bioshogi
         {
           key: "相居飛車",
           func: -> {
-            if @container.players.all? { |e| e.tag_bundle.include?("居飛車") }
-              @container.players.each do |e|
+            if container.players.all? { |e| e.tag_bundle.include?("居飛車") }
+              container.players.each do |e|
                 e.tag_bundle << "相居飛車"
               end
             end
@@ -55,7 +55,7 @@ module Bioshogi
         # {
         #   key: "対居飛車",
         #   func: -> {
-        #     @container.players.each do |e|
+        #     container.players.each do |e|
         #       if e.opponent_player.tag_bundle.include?("居飛車")
         #         e.tag_bundle << "対居飛車"
         #       end
@@ -65,8 +65,8 @@ module Bioshogi
         {
           key: "相振り飛車",
           func: -> {
-            if @container.players.all? { |e| e.tag_bundle.include?("振り飛車") }
-              @container.players.each do |e|
+            if container.players.all? { |e| e.tag_bundle.include?("振り飛車") }
+              container.players.each do |e|
                 e.tag_bundle << "相振り飛車"
               end
             end
@@ -76,10 +76,10 @@ module Bioshogi
           key: "対抗形",
           description: "片方だけが「振り飛車」なら両方に「対抗形」",
           func: -> {
-            if player = @container.players.find { |e| e.tag_bundle.include?("振り飛車") }
-              others = @container.players - [player]
+            if player = container.players.find { |e| e.tag_bundle.include?("振り飛車") }
+              others = container.players - [player]
               if others.none? { |e| e.tag_bundle.include?("振り飛車") }
-                @container.players.each do |e|
+                container.players.each do |e|
                   e.tag_bundle << "対抗形"
                 end
               end
@@ -92,7 +92,7 @@ module Bioshogi
         {
           key: "○○系",
           func: -> {
-            # @container.players.each do |e|
+            # container.players.each do |e|
             #   items = e.tag_bundle.attack_infos.unwant_rejected_ancestors
             #   CategoryInfo.each do |c|
             #     if items.any? { |e| e.root == c.object }
@@ -102,7 +102,7 @@ module Bioshogi
             #   end
             # end
 
-            # @container.players.each do |e|
+            # container.players.each do |e|
             #   e.tag_bundle.to_all_flat_array.each do |item|
             #     if item.respond_to?(:category)
             #       if v = item.category
@@ -127,8 +127,8 @@ module Bioshogi
           key: "居玉",
           description: "一度も動かなかった or または戦いが激しくなってから動いた",
           func: -> {
-            @container.players.each do |e|
-              if e.king_first_moved_turn.nil? || e.king_first_moved_turn >= @container.outbreak_turn
+            container.players.each do |e|
+              if e.king_first_moved_turn.nil? || e.king_first_moved_turn >= container.outbreak_turn
                 e.tag_bundle << "居玉"
               end
             end
@@ -137,8 +137,8 @@ module Bioshogi
         {
           key: "相居玉",
           func: -> {
-            if @container.players.all? { |e| e.tag_bundle.include?("居玉") }
-              @container.players.each do |e|
+            if container.players.all? { |e| e.tag_bundle.include?("居玉") }
+              container.players.each do |e|
                 e.tag_bundle << "相居玉"
               end
             end
@@ -150,12 +150,12 @@ module Bioshogi
         {
           key: "急戦・持久戦",
           func: -> {
-            if @container.outbreak_turn < Stat::OUTBREAK_TURN_AVG
+            if container.outbreak_turn < Stat::OUTBREAK_TURN_AVG
               tag = "急戦"
             else
               tag = "持久戦"
             end
-            @container.players.each do |e|
+            container.players.each do |e|
               e.tag_bundle << tag
             end
           },
@@ -163,12 +163,12 @@ module Bioshogi
         {
           key: "短手数・長手数",
           func: -> {
-            if @container.turn_info.turn_offset < Stat::TURN_MAX_AVG
+            if container.turn_info.turn_offset < Stat::TURN_MAX_AVG
               tag = "短手数"
             else
               tag = "長手数"
             end
-            @container.players.each do |e|
+            container.players.each do |e|
               e.tag_bundle << tag
             end
           },
@@ -181,7 +181,7 @@ module Bioshogi
           description: "大駒がない状態で勝った場合",
           func: -> {
             if win_side_location
-              player = @container.player_at(win_side_location)
+              player = container.player_at(win_side_location)
               if player.strong_piece_have_count.zero?         # 最後の状態でも全ブッチ状態なら
                 if player.tag_bundle.include?("大駒全ブッチ") # 途中、大駒全ブッチしいて (←これがないと 相入玉.kif でも入ってしまう)
                   player.tag_bundle << "屍の舞"
@@ -201,8 +201,8 @@ module Bioshogi
           key: "相穴熊",
           func: -> {
             tag = Analysis::TagIndex.fetch("穴熊")
-            if @container.players.all? { |e| e.tag_bundle.include?(tag) }
-              @container.players.each do |player|
+            if container.players.all? { |e| e.tag_bundle.include?(tag) }
+              container.players.each do |player|
                 player.tag_bundle << "相穴熊"
               end
             end
@@ -212,8 +212,8 @@ module Bioshogi
           key: "相入玉",
           func: -> {
             tag = Analysis::TagIndex.fetch("入玉")
-            if @container.players.all? { |e| e.tag_bundle.include?(tag) }
-              @container.players.each do |player|
+            if container.players.all? { |e| e.tag_bundle.include?(tag) }
+              container.players.each do |player|
                 player.tag_bundle << "相入玉"
               end
             end
@@ -224,7 +224,7 @@ module Bioshogi
           key: "ロケット",
           func: -> {
             group_info = Analysis::GroupInfo["ロケット"]
-            @container.players.each do |e|
+            container.players.each do |e|
               if e.tag_bundle.all_tags.any? { |e| e.group_info == group_info }
                 e.tag_bundle << "ロケット"
               end
@@ -237,11 +237,11 @@ module Bioshogi
         {
           key: "都詰め",
           func: -> {
-            if soldier = @container.board["55"]
+            if soldier = container.board["55"]
               if soldier.piece.key == :king
-                if @container.lose_player.location == soldier.location
-                  @container.win_player.tag_bundle << "都詰め"
-                  if hand_log = @container.hand_logs.last
+                if container.lose_player.location == soldier.location
+                  container.win_player.tag_bundle << "都詰め"
+                  if hand_log = container.hand_logs.last
                     hand_log.tag_bundle << "都詰め"
                   end
                 end
@@ -252,9 +252,9 @@ module Bioshogi
         {
           key: "吊るし桂",
           func: -> {
-            if hand_log = @container.hand_logs.last
+            if hand_log = container.hand_logs.last
               if hand_log.soldier.piece.key == :knight
-                @container.win_player.tag_bundle << "吊るし桂"
+                container.win_player.tag_bundle << "吊るし桂"
                 hand_log.tag_bundle << "吊るし桂"
               end
             end
@@ -263,11 +263,11 @@ module Bioshogi
         {
           key: "雪隠詰め",
           func: -> {
-            if @container.params[:input_last_action_info]&.last_checkmate_p
-              soldier = @container.lose_player.king_soldier
+            if container.params[:input_last_action_info]&.last_checkmate_p
+              soldier = container.lose_player.king_soldier
               if soldier.bottom_spaces == 0 && soldier.side_edge?
-                @container.win_player.tag_bundle << "雪隠詰め"
-                if hand_log = @container.hand_logs.last
+                container.win_player.tag_bundle << "雪隠詰め"
+                if hand_log = container.hand_logs.last
                   hand_log.tag_bundle << "雪隠詰め"
                 end
               end
